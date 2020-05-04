63f53cbf4be97a4265a0a9b195709e0f6429f994
alacuku <aldo.lacu@live.it>
2020-05-04 19:20:54 +0200

    TunnelEndpointCreatorOperator updated to work with the lates API version of Advertisement CRD
    Changes:
      The tunnelEndpoint CR is created in the same namespace as the Advertisement CR
      The tunnelEndpoint name is derived from the Advertisement CR
    
    Optimization:
      Removed the in memory data structure that keeps the correlation between advertisemen CR and tunnelEndpoint Cr, but for a given advertisment CR we can always derive the associated tunnelEndpoint CR

--- internal/advertisement-operator/tunnelEndpointCreator-operator.go
+++ internal/advertisement-operator/tunnelEndpointCreator-operator.go
@@ -17,7 +17,9 @@ package advertisement_operator
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-logr/logr"
+	dronetOperator "github.com/netgroup-polito/dronev2/pkg/dronet-operator"
 	"github.com/pkg/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -31,6 +33,10 @@ import (
 	protocolv1 "github.com/netgroup-polito/dronev2/api/advertisement-operator/v1"
 	dronetv1 "github.com/netgroup-polito/dronev2/api/tunnel-endpoint/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+)
+
+const (
+	tunEndpointNameSuffix = "-tunendpoint"
 )
 
 // AdvertisementReconciler reconciles a Advertisement object
@@ -51,24 +57,59 @@ type TunnelEndpointCreator struct {
 func (r *TunnelEndpointCreator) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
 	log := r.Log.WithValues("tunnelEndpointCreator-controller", req.NamespacedName)
-
+	tunnelEndpointCreatorFinalizer := "tunnelEndpointCreator-Finalizer.dronet.drone.com"
 	// get advertisement
 	var adv protocolv1.Advertisement
 	if err := r.Get(ctx, req.NamespacedName, &adv); err != nil {
 		// reconcile was triggered by a delete request
 		log.Info("Advertisement " + req.Name + " deleted")
-		if client.IgnoreNotFound(err) == nil {
-			err := r.deleteTunEndpoint(req.NamespacedName)
-			if err != nil {
-				log.Error(err, "failed to delete the endpointTunnel CR associated to the advertisment", "advKey", req.NamespacedName.String())
-			}
-		}
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-	err := r.createOrUpdateTunEndpoint(&adv, req.NamespacedName)
-	if err != nil {
-		log.Error(err, "error while creating or updating")
+	// examine DeletionTimestamp to determine if object is under deletion
+	if adv.ObjectMeta.DeletionTimestamp.IsZero() {
+		if !dronetOperator.ContainsString(adv.ObjectMeta.Finalizers, tunnelEndpointCreatorFinalizer) {
+			// The object is not being deleted, so if it does not have our finalizer,
+			// then lets add the finalizer and update the object. This is equivalent
+			// registering our finalizer.
+			adv.ObjectMeta.Finalizers = append(adv.Finalizers, tunnelEndpointCreatorFinalizer)
+			if err := r.Update(ctx, &adv); err != nil {
+				//while updating we check if the a resource version conflict happened
+				//which means the version of the object we have is outdated.
+				//a solution could be to return an error and requeue the object for later process
+				//but if the object has been changed by another instance of the controller running in
+				//another host it already has been put in the working queue so decide to forget the
+				//current version and process the next item in the queue assured that we handle the object later
+				if apierrors.IsConflict(err) {
+					return ctrl.Result{}, nil
+				}
+				log.Error(err, "unable to update adv", adv.Name)
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		//the object is being deleted
+		if dronetOperator.ContainsString(adv.Finalizers, tunnelEndpointCreatorFinalizer) {
+			if err := r.deleteTunEndpoint(&adv); err != nil {
+				log.Error(err, "error while deleting endpoint")
+				return ctrl.Result{}, err
+			}
+			//remove the finalizer from the list and update it.
+			adv.Finalizers = dronetOperator.RemoveString(adv.Finalizers, tunnelEndpointCreatorFinalizer)
+			if err := r.Update(ctx, &adv); err != nil {
+				if apierrors.IsConflict(err) {
+					return ctrl.Result{}, nil
+				}
+				log.Error(err, "unable to update adv %s", adv.Name, "in namespace", adv.Namespace)
+				return ctrl.Result{}, err
+			}
+		}
 		return ctrl.Result{}, nil
+	}
+
+	err := r.createTunEndpoint(&adv)
+	if err != nil {
+		log.Error(err, "error while creating endpoint")
+		return ctrl.Result{}, err
 	}
 	return ctrl.Result{}, nil
 }
@@ -158,7 +199,7 @@ func (r *TunnelEndpointCreator) createOrUpdateTunEndpoint(adv *protocolv1.Advert
 			}
 		}
 	} else if err.Error() == "notFound" {
-		err := r.createTunEndpoint(adv, advKey)
+		err := r.createTunEndpoint(adv)
 		if err == nil {
 			return nil
 		} else {
@@ -200,82 +241,79 @@ func (r *TunnelEndpointCreator) updateTunEndpoint(adv *protocolv1.Advertisement,
 	}
 }
 
-func (r *TunnelEndpointCreator) createTunEndpoint(adv *protocolv1.Advertisement, advKey types.NamespacedName) error {
+//we do not support updates to the ADV CR by the user, at least not yet
+//we assume that the ADV is created by the Remote Server and is the only one who can remove or update it
+//if the ADV has to be updated first we remove it and then recreate the it with new values
+//For each ADV CR a tunnelEndpoint CR is created in the same namespace as the ADV CR and the name of the later
+//is derived from the name of the ADV adding a suffix. Doing so, given an ADV we can always create, update or delete
+//the associated tunnelEndpoint CR without the necessity for its key to be saved by the operator.
+func (r *TunnelEndpointCreator) createTunEndpoint(adv *protocolv1.Advertisement) error {
 	funcName := "createTunEndpoint"
 	ctx := context.Background()
 	log := r.Log.WithValues("tunnelEndpointCreator-controller", funcName)
-	namespace, err := r.getNamespace()
-	if err != nil {
-		return err
+	var tunEndpoint dronetv1.TunnelEndpoint
+
+	//build the key used to retrieve the tunnelEndpoint CR
+	tunEndKey := types.NamespacedName{
+		Namespace: adv.Namespace,
+		Name:      adv.Name + tunEndpointNameSuffix,
 	}
-	//TODO: implement the IPAM algorithm
-	tunEndpoint := &dronetv1.TunnelEndpoint{
-		ObjectMeta: v1.ObjectMeta{
-			//the name is derived from the clusterID
-			Name: adv.Spec.ClusterId + "-tunnendpoint",
-			//the namespace is read from the Environment variable passe to the pod
-			Namespace: namespace,
-		},
-		Spec: dronetv1.TunnelEndpointSpec{
-			ClusterID:       adv.Spec.ClusterId,
-			PodCIDR:         adv.Spec.Network.PodCIDR,
-			TunnelPublicIP:  adv.Spec.Network.GatewayIP,
-			TunnelPrivateIP: adv.Spec.Network.GatewayPrivateIP,
-		},
-		Status: dronetv1.TunnelEndpointStatus{},
-	}
-	err = r.Create(ctx, tunEndpoint)
-	if err == nil {
-		log.Info("created the custom resource", "name", tunEndpoint.Name, "namespace", tunEndpoint.Namespace, "clusterId", adv.Spec.ClusterId, "podCIDR", adv.Spec.Network.PodCIDR, "gatewayPublicIP", adv.Spec.Network.GatewayIP, "tunnelPrivateIP", adv.Spec.Network.GatewayPrivateIP)
-		adv.Status.TunnelEndpointKey = protocolv1.NamespacedName{
-			Namespace: tunEndpoint.Namespace,
-			Name:      tunEndpoint.Name,
+	//retrieve the tunnelEndpoint CR
+	err := r.Get(ctx, tunEndKey, &tunEndpoint)
+	//if the CR exist then do nothing and return
+	if err == nil{
+		return nil
+	} else if apierrors.IsNotFound(err){
+		//if tunnelEndpoint referenced by the key does not exist then we create it
+		//TODO: implement the IPAM algorithm
+		tunEndpoint := &dronetv1.TunnelEndpoint{
+			ObjectMeta: v1.ObjectMeta{
+				//the name is derived from the clusterID
+				Name: adv.Name + tunEndpointNameSuffix,
+				//the namespace is read from the Environment variable passe to the pod
+				Namespace: adv.Namespace,
+			},
+			Spec: dronetv1.TunnelEndpointSpec{
+				ClusterID:       adv.Spec.ClusterId,
+				PodCIDR:         adv.Spec.Network.PodCIDR,
+				TunnelPublicIP:  adv.Spec.Network.GatewayIP,
+				TunnelPrivateIP: adv.Spec.Network.GatewayPrivateIP,
+			},
+			Status: dronetv1.TunnelEndpointStatus{},
 		}
-		//add the value to the map
-		r.TunnelEndpointMap[advKey.String()] = types.NamespacedName(adv.Status.TunnelEndpointKey)
-		err := r.Client.Status().Update(ctx, adv)
-		if err == nil {
-			log.Info("updated the status of ADV custom resource", "tunnelEndpointKey.Namespace", tunEndpoint.Namespace, "tunnelEndpointKey.Name", tunEndpoint.Name)
-			return nil
-		} else {
-			log.Info("failed to update the status of ADV custom resource", "tunnelEndpointKey.Namespace", tunEndpoint.Namespace, "tunnelEndpointKey.Name", tunEndpoint.Name)
+		err = r.Create(ctx, tunEndpoint)
+		if err != nil {
+			log.Info("failed to create the custom resource", "name", tunEndpoint.Name, "namespace", tunEndpoint.Namespace, "clusterId", adv.Spec.ClusterId, "podCIDR", adv.Spec.Network.PodCIDR, "gatewayPublicIP", adv.Spec.Network.GatewayIP, "tunnelPrivateIP", adv.Spec.Network.GatewayPrivateIP)
 			return err
 		}
-	} else {
-		log.Info("failed to create the custom resource", "name", tunEndpoint.Name, "namespace", tunEndpoint.Namespace, "clusterId", adv.Spec.ClusterId, "podCIDR", adv.Spec.Network.PodCIDR, "gatewayPublicIP", adv.Spec.Network.GatewayIP, "tunnelPrivateIP", adv.Spec.Network.GatewayPrivateIP)
+		log.Info("created the custom resource", "name", tunEndpoint.Name, "namespace", tunEndpoint.Namespace, "clusterId", adv.Spec.ClusterId, "podCIDR", adv.Spec.Network.PodCIDR, "gatewayPublicIP", adv.Spec.Network.GatewayIP, "tunnelPrivateIP", adv.Spec.Network.GatewayPrivateIP)
+		return nil
+	}else {
 		return err
 	}
 }
 
-func (r *TunnelEndpointCreator) deleteTunEndpoint(advKey types.NamespacedName) error {
-	funcName := "deleteTunEndpoint"
+func (r *TunnelEndpointCreator) deleteTunEndpoint(adv *protocolv1.Advertisement) error {
 	ctx := context.Background()
-	log := r.Log.WithValues("tunnelEndpointCreator-controller", funcName)
-	tunEndpointKey, found := r.TunnelEndpointMap[advKey.String()]
-	if found {
-		tunEndpoint, err := r.getTunnelEndpointByKey(tunEndpointKey)
-		if err == nil {
-			err := r.Delete(ctx, tunEndpoint)
-			if err == nil {
-				log.Info("deleted tunnelEndopoint", "name", tunEndpoint.Name)
-				//remove the entry from the map
-				delete(r.TunnelEndpointMap, advKey.String())
-				return nil
-			} else {
-				log.Info("failed to delete tunnelEndopoint", "name", tunEndpoint.Name)
-				return err
-			}
-		} else if apierrors.IsNotFound(err) {
-			log.Info("the required tunnelEndpoint CR is not found", "key", tunEndpointKey.String())
-			//remove the entry from the map
-			delete(r.TunnelEndpointMap, advKey.String())
+	var tunEndpoint dronetv1.TunnelEndpoint
+	//build the key used to retrieve the tunnelEndpoint CR
+	tunEndKey := types.NamespacedName{
+		Namespace: adv.Namespace,
+		Name:      adv.Name + tunEndpointNameSuffix,
+	}
+	//retrieve the tunnelEndpoint CR
+	err := r.Get(ctx, tunEndKey, &tunEndpoint)
+	//if the CR exist then do nothing and return
+	if err == nil{
+		err := r.Delete(ctx, &tunEndpoint)
+		if err != nil{
+			return fmt.Errorf("unable to delete endpoint %s in namespace %s : %v", tunEndpoint.Name, tunEndpoint.Namespace, err)
+		}else{
 			return nil
-		} else {
-			log.Info("failed to retrieve the tunnelEndopint CR", "key", tunEndpointKey.String())
-			return err
 		}
-	} else {
-		log.Info("no tunnelEndpoint CR related to adv was found", "advKey", advKey.String())
+	} else if apierrors.IsNotFound(err){
 		return nil
+	}else {
+		return fmt.Errorf("unable to get endpoint with key %s: %v", tunEndKey.String(), err)
 	}
 }
