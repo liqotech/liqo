e6bd0b710b996bd434e466f7207bda3b6dc3cb89
mLavacca <lavacca.mattia@gmail.com>
2020-07-02 10:55:25 +0200

    e2e test pipeline added

---
+++ scripts/utils/get_branch_owner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import requests
+import json
+import sys
+
+if len(sys.argv) != 2:
+    exit(1)
+
+url = "https://api.github.com/repos/LiqoTech/liqo/pulls/" + sys.argv[1] + "/commits"
+
+res = requests.get(url)
+data = (json.loads(res.content))
+print(data[0]['author']['login'])
