b7bfccd7ece2ccb6b4ccaf4cd7769e4803e85aef
Alex Palesandro <alex.palesandro@gmail.com>
2020-07-14 13:59:57 +0200

    Update Installer
    This PR adds the version customization via environment variable for the installer.
    This can be used to test a specific liqo, version by setting the corresponding suffix
    and version.

--- install.sh
+++ install.sh
@@ -77,6 +77,12 @@ HELM_ARCHIVE=helm-${HELM_VERSION}-linux-amd64.tar.gz
 HELM_URL=https://get.helm.sh/$HELM_ARCHIVE
 DEFAULT_GATEWAY_PRIVATE_IP=192.168.1.1
 NAMESPACE_DEFAULT="liqo"
+# The following variable are used a default value to select the images when installing LIQO.
+# When installing a non released version:
+# - Export LIQO_SUFFIX="-ci" if the commit is not on the master branch
+# - Export LIQO_VERSION to the id of your commit
+LIQO_VERSION_DEFAULT="latest"
+LIQO_SUFFIX_DEFAULT=""
 
 # Necessary Commands
 commands="curl kubectl"
@@ -96,7 +102,15 @@ mkdir -p $TMPDIR/bin/
 echo "[PRE-INSTALL] [HELM] Checking HELM installation..."
 echo "[PRE-INSTALL] [HELM]: Downloading Helm $HELM_VERSION"
 curl --fail -L ${HELM_URL} | tar zxf - --directory="$TMPDIR/bin/" --wildcards '*/helm' --strip 1
-git clone "$URL" $TMPDIR/liqo --depth 1
+if [ "$LIQO_SUFFIX" == "-ci" ] && [ -z "${LIQO_VERSION}" ]  ; then
+  git clone "$URL" $TMPDIR/liqo
+  cd  $TMPDIR/liqo
+  git checkout "$LIQO_VERSION" > /dev/null 2> /dev/null
+  cd -
+else
+  git clone "$URL" $TMPDIR/liqo --depth 1
+fi
+
 
 echo "[PRE-INSTALL]: Collecting installation variables. The installer will retrieve installation parameters from your
  Kubernetes cluster. Feel free to override them, by launching it with those environment variables set in advance."
@@ -117,12 +131,16 @@ GATEWAY_PRIVATE_IP_COMMAND="echo $DEFAULT_GATEWAY_PRIVATE_IP"
 set_variable_from_command GATEWAY_PRIVATE_IP GATEWAY_PRIVATE_IP_COMMAND "[ERROR]: Unable to set Gateway Private IP"
 NAMESPACE_COMMAND="echo $NAMESPACE_DEFAULT"
 set_variable_from_command NAMESPACE NAMESPACE_COMMAND "[ERROR]: Error while creating the namespace... "
+LIQO_SUFFIX_COMMAND="echo $LIQO_SUFFIX_DEFAULT"
+set_variable_from_command LIQO_SUFFIX LIQO_SUFFIX_COMMAND "[ERROR]: Error setting the Liqo suffix... "
+LIQO_VERSION_COMMAND="echo $LIQO_VERSION_DEFAULT"
+set_variable_from_command LIQO_VERSION LIQO_VERSION_COMMAND "[ERROR]: Error setting the Liqo version... "
+
 
 #Wait for the installation to complete
-echo $NAMESPACE
 kubectl create ns $NAMESPACE
 $TMPDIR/bin/helm dependency update $TMPDIR/liqo/deployments/liqo_chart
-$TMPDIR/bin/helm install liqo -n liqo $TMPDIR/liqo/deployments/liqo_chart --set podCIDR=$POD_CIDR --set serviceCIDR=$SERVICE_CIDR --set gatewayPrivateIP=$GATEWAY_PRIVATE_IP --set gatewayIP=$GATEWAY_IP
+$TMPDIR/bin/helm install liqo -n liqo $TMPDIR/liqo/deployments/liqo_chart --set podCIDR=$POD_CIDR --set serviceCIDR=$SERVICE_CIDR --set gatewayPrivateIP=$GATEWAY_PRIVATE_IP --set gatewayIP=$GATEWAY_IP --set global.suffix="$LIQO_SUFFIX" --set global.version="$LIQO_VERSION"
 echo "[INSTALL]: Installing LIQO on your cluster..."
 sleep 30
 
