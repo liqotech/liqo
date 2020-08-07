8001c04ab57bfc60e25edd05c05591c83779c855
Francesco Borgogni <francesco.borgogni4@gmail.com>
2020-08-07 15:41:50 +0200

    Added advertisement status in PeeringRequest
    When the adv is accepted/refused by the foreign cluster, the status is copied in the local PeeringRequest to notify home cluster

--- test/unit/advertisement-operator/remote-watcher_test.go
+++ test/unit/advertisement-operator/remote-watcher_test.go
@@ -2,6 +2,7 @@ package advertisement_operator
 
 import (
 	protocolv1 "github.com/liqoTech/liqo/api/advertisement-operator/v1"
+	discoveryv1 "github.com/liqoTech/liqo/api/discovery/v1"
 	advertisement_operator "github.com/liqoTech/liqo/internal/advertisement-operator"
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -9,7 +10,7 @@ import (
 	"time"
 )
 
-func TestWatchAdvertisement(t *testing.T) {
+func TestWatchAdvertisementNetworkRemapping(t *testing.T) {
 	// prepare resources for advertisement
 	clusterConfig := createFakeClusterConfig()
 	b := createBroadcaster(clusterConfig.Spec)
@@ -31,7 +32,7 @@ func TestWatchAdvertisement(t *testing.T) {
 	time.Sleep(1 * time.Second)
 
 	// after having created home adv on foreign cluster, start watching it
-	go advertisement_operator.WatchAdvertisement(b.LocalClient, b.RemoteClient, homeAdv.Name, foreignAdv.Name)
+	go b.WatchAdvertisement(homeAdv.Name, foreignAdv.Name)
 
 	// set home cluster adv status and update it: this will trigger the watcher
 	newPodCIDR := "1.2.3.4/16"
@@ -52,4 +53,55 @@ func TestWatchAdvertisement(t *testing.T) {
 
 	err = b.RemoteClient.Resource("advertisements").Delete(homeAdv.Name, v1.DeleteOptions{})
 	assert.Nil(t, err)
+}
+
+func TestWatchAdvertisementAcceptance(t *testing.T) {
+	// prepare resources for advertisement
+	clusterConfig := createFakeClusterConfig()
+	b := createBroadcaster(clusterConfig.Spec)
+
+	// create fake peering request on cluster home
+	pr := discoveryv1.PeeringRequest{
+		TypeMeta: v1.TypeMeta{},
+		ObjectMeta: v1.ObjectMeta{
+			Name: b.PeeringRequestName,
+		},
+		Spec: discoveryv1.PeeringRequestSpec{
+			ClusterID:     b.PeeringRequestName,
+			Namespace:     "test",
+			KubeConfigRef: nil,
+		},
+		Status: discoveryv1.PeeringRequestStatus{},
+	}
+	_, err := b.DiscoveryClient.Resource("peeringrequests").Create(&pr, v1.CreateOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create fake advertisement on cluster foreign
+	homeAdv := prepareAdv(b)
+	_, err = b.RemoteClient.Resource("advertisements").Create(&homeAdv, v1.CreateOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(1 * time.Second)
+
+	// after having created home adv on foreign cluster, start watching it
+	go b.WatchAdvertisement(homeAdv.Name, "")
+
+	// set adv status and update it: this will trigger the watcher
+	homeAdv.Status.AdvertisementStatus = advertisement_operator.AdvertisementAccepted
+	_, err = b.RemoteClient.Resource("advertisements").Update(homeAdv.Name, &homeAdv, v1.UpdateOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(5 * time.Second)
+
+	tmp, err := b.DiscoveryClient.Resource("peeringrequests").Get(b.PeeringRequestName, v1.GetOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	pr2 := tmp.(*discoveryv1.PeeringRequest)
+	assert.Equal(t, advertisement_operator.AdvertisementAccepted, pr2.Status.AdvertisementStatus)
 }
