762a444b73236994c700b623df1bc2341a873f4c
nappozord <nappozord@users.noreply.github.com>
2020-08-12 15:42:40 +0200

    Add liqo kind installer
    Co-authored-by: AbakusW <camix1984@gmail.com>

--- test/kind/kind.sh
+++ test/kind/kind.sh
@@ -1,78 +1,26 @@
 #!/bin/bash
 
-configKindCluster(){
-  i=$1
-  port=$((30000+"$i"))
-  cat << EOF > cluster"$i"-config.yaml
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-networking:
-  apiServerAddress: "$localIP"
-  apiServerPort: $port
-  podSubnet: "10.${i}00.0.0/16"
-  serviceSubnet: "10.9$i.0.0/12"
-nodes:
-  - role: control-plane
-    image: kindest/node:v1.18.2
-EOF
-  kind create cluster --name cluster"$i" --kubeconfig kubeconfig-cluster"$i" --config cluster"$i"-config.yaml --wait 2m
-}
-
-if [ $# -ne 1 ]
-then
-  echo "please, provide your local IP address"
-  exit 1
-fi
-
-# do not set localhost, use your local IP address
-localIP=$1
-clusterNum=2
-
-#delete all clusters
-for ((i=1;i<=clusterNum;i++)); do
-  kind delete cluster --name cluster"$i"
-done
-
-# create clusters
-for ((i=1;i<=clusterNum;i++)); do
-  configKindCluster $i
-  pids[${i}]=$!
-done
-
-# wait for all pids
-for pid in ${pids[*]}; do
-    wait $pid
-done
-
-# configure clusters installing the advertisement crd and creating the configMaps containing the kubeconfig
-for ((i=1;i<=clusterNum;i++)); do
-  export KUBECONFIG=kubeconfig-cluster${i}
-  kubectl apply -f adv-crd.yaml
-  kubectl apply -f sn-crd.yaml
-  for ((j=1;j<=clusterNum;j++)); do
-    if [ $i -eq $j ] ; then
-      continue
-    fi
-    id=cluster${j}
-    #kubectl config view --flatten --minify >>kubeconfig-${id}
-    kubectl create configmap foreign-kubeconfig-${id} --from-file=remote=kubeconfig-${id}
-  done
-done
-
-# create advertisement-operator deployment
-for ((i=1;i<=clusterNum;i++)); do
-  export KUBECONFIG=kubeconfig-cluster${i}
-  sed -i -e "s/clusterX/cluster$i/g" adv-operator_cm.yaml
-  sed -i -e "s/0.0.0.0/172.17.0.$i/g" adv-operator_cm.yaml
-  sed -i -e "s/1.2.3.4/192.168.0.$i/g" adv-operator_cm.yaml
-  kubectl apply -f adv-operator_cm.yaml
-  sed -i -e "s/cluster$i/clusterX/g" adv-operator_cm.yaml
-  sed -i -e "s/172.17.0.$i/0.0.0.0/g" adv-operator_cm.yaml
-  sed -i -e "s/192.168.0.$i/1.2.3.4/g" adv-operator_cm.yaml
-  kubectl apply -f adv-deploy.yaml
-  kubectl apply -f broadcaster-deploy.yaml
-  kubectl apply -f sn-deploy.yaml
-done
+kind delete clusters liqo1 liqo2
+echo create cluster liqo1...
+kind create cluster --name liqo1 --kubeconfig liqo_kubeconf_1 --config liqo-cluster-config.yaml --wait 2m
+export KUBECONFIG=$(pwd)/liqo_kubeconf_1
+kubectl label no liqo1-control-plane liqonet.liqo.io/gateway=true
+echo liqo1 installed and configured
+echo installing liqo...
+curl https://raw.githubusercontent.com/LiqoTech/liqo/master/install.sh | bash
+echo COMPLETED
+echo ----------------
+echo create cluster liqo2...
+kind create cluster --name liqo2 --kubeconfig liqo_kubeconf_2 --config liqo-cluster-config.yaml --wait 2m
+export KUBECONFIG=$(pwd)/liqo_kubeconf_2
+kubectl label no liqo2-control-plane liqonet.liqo.io/gateway=true
+echo liqo2 installed and configured
+echo installing liqo...
+curl https://raw.githubusercontent.com/LiqoTech/liqo/master/install.sh | bash
+echo COMPLETED
+echo ----------------
+echo "liqo1 KUBECONFIG=$(pwd)/liqo_kubeconf_1"
+echo "liqo2 KUBECONFIG=$(pwd)/liqo_kubeconf_2"
 
 exit 0
 
