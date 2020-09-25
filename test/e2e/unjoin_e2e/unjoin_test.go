ffc5265a1c27f7ad5c2db473f662db64dd0ddef2
Alessandro Olivero <aleoli96@gmail.com>
2020-09-25 11:03:27 +0200

    add uninstall e2e test
    adds e2e test fo Liqo uninstall

---
+++ test/e2e/unjoin_e2e/unjoin_test.go
@@ -0,0 +1,39 @@
+package unjoin_e2e
+
+import (
+	context2 "context"
+	"github.com/liqotech/liqo/test/e2e/util"
+	"gotest.tools/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog"
+	"testing"
+)
+
+func TestUnjoin(t *testing.T) {
+	context := util.GetTester()
+	NoPods(context.Client1, context.Namespace, t, "cluster1")
+
+	NoJoined(context.Client2, t, "cluster2")
+	util.ArePodsUp(context.Client2, context.Namespace, t, "cluster2")
+}
+
+func NoPods(clientset *kubernetes.Clientset, namespace string, t *testing.T, clustername string) {
+	pods, err := clientset.CoreV1().Pods(namespace).List(context2.TODO(), metav1.ListOptions{})
+	if err != nil {
+		klog.Error(err)
+		t.Fail()
+	}
+	assert.Equal(t, len(pods.Items), 0, "There are still running pods on "+clustername)
+}
+
+func NoJoined(clientset *kubernetes.Clientset, t *testing.T, clustername string) {
+	nodes, err := clientset.CoreV1().Nodes().List(context2.TODO(), metav1.ListOptions{
+		LabelSelector: "type=virtual-node",
+	})
+	if err != nil {
+		klog.Error(err)
+		t.Fail()
+	}
+	assert.Equal(t, len(nodes.Items), 0, "There are still virtual nodes on "+clustername)
+}
