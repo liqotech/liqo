7071541c516784dbdd2d3fa2eda2c7b7d38950f6
Alessandro Olivero <aleoli96@gmail.com>
2020-10-30 11:09:51 +0100

    reflect ServiceAccount token secrets

--- test/unit/reflection/secrets_test.go
+++ test/unit/reflection/secrets_test.go
@@ -27,6 +27,33 @@ func TestSecretAdd(t *testing.T) {
 	assert.Equal(t, postadd.Namespace, "test")
 }
 
+func TestSASecretAdd(t *testing.T) {
+	secretsReflector := InitTest("secrets")
+
+	secret := v1.Secret{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "name",
+			Namespace: "namespace",
+			Annotations: map[string]string{
+				"kubernetes.io/service-account.name": "test-sa",
+				"kubernetes.io/service-account.uid":  "test-uid",
+			},
+		},
+		Data: map[string][]byte{
+			"thesecret": []byte("ILoveLiqo"),
+		},
+		Type: v1.SecretTypeServiceAccountToken,
+	}
+
+	postadd := secretsReflector.PreProcessAdd(&secret).(*v1.Secret)
+
+	assert.Equal(t, postadd.Namespace, "test")
+	assert.Assert(t, len(postadd.Annotations) == 0, "service account annotation are not removed")
+	assert.Equal(t, postadd.Type, v1.SecretTypeOpaque)
+	assert.Equal(t, postadd.Labels["kubernetes.io/service-account.name"], "test-sa", "service account reference label is not set correctly")
+}
+
 func TestSecretUpdate(t *testing.T) {
 	secretsReflector := InitTest("secrets")
 
