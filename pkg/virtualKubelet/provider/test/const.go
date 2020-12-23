a0652a9a7375c082b163da76c382e576fe4106bd
mlavacca <lavacca.mattia@gmail.com>
2020-12-23 09:02:12 +0100

    Old vk tests deletion
    This commit deletes some useless old tests

--- pkg/virtualKubelet/provider/test/const.go
+++ pkg/virtualKubelet/provider/test/const.go
@@ -1,24 +1,8 @@
 package test
 
-import (
-	"github.com/liqotech/liqo/internal/liqonet/tunnelEndpointCreator"
-	pkg "github.com/liqotech/liqo/pkg/virtualKubelet"
-	"time"
-)
-
 const (
-	Namespace             = "test"
-	NattedNamespace       = Namespace + "-" + HomeClusterId
-	HostName              = "testHost"
-	NodeName              = "testNode"
-	AdvName               = pkg.AdvertisementPrefix + ForeignClusterId
-	TepName               = tunnelEndpointCreator.TunEndpointNamePrefix + ForeignClusterId
-	EndpointsName         = "testEndpoints"
-	HomeClusterId         = "homeClusterID"
-	ForeignClusterId      = "foreignClusterID"
-	PodCIDR               = "1.2.3.4/16"
-	RemoteRemappedPodCIDR = "5.6.7.8/16"
-	TunnelPublicIP        = "5.6.7.8"
-	LocalRemappedPodCIDR  = "100.200.0.0/16"
-	Timeout               = 10 * time.Second
+	Namespace        = "test"
+	EndpointsName    = "testEndpoints"
+	HomeClusterId    = "homeClusterID"
+	ForeignClusterId = "foreignClusterID"
 )
