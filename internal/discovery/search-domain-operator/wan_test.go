5e2f7bfac2b640af66804a42fb836adc232956b0
Alessandro Olivero <aleoli96@gmail.com>
2021-01-08 11:47:40 +0100

    dns discovery testing

---
+++ internal/discovery/search-domain-operator/wan_test.go
@@ -0,0 +1,45 @@
+package search_domain_operator
+
+import (
+	"github.com/liqotech/liqo/internal/discovery"
+	"github.com/liqotech/liqo/pkg/testUtils"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestWan(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Wan Suite")
+}
+
+var _ = Describe("Wan", func() {
+
+	var (
+		dnsServer testUtils.DnsServer
+	)
+
+	BeforeSuite(func() {
+		dnsServer = testUtils.DnsServer{}
+		dnsServer.Serve()
+	})
+
+	AfterSuite(func() {
+		dnsServer.Shutdown()
+	})
+
+	Context("Wan", func() {
+
+		It("resolve Wan", func() {
+			data, err := LoadAuthDataFromDNS(dnsServer.GetAddr(), dnsServer.GetName())
+			Expect(err).To(BeNil())
+			Expect(data).NotTo(BeNil())
+			Expect(data).To(Equal([]*discovery.AuthData{
+				discovery.NewAuthData("h1.test.liqo.io.", 1234, 60),
+				discovery.NewAuthData("h2.test.liqo.io.", 4321, 60),
+			}))
+		})
+
+	})
+
+})
