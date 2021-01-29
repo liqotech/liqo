28a4d0ea9a4d9a00ab3b2d9bb37f8cde7d785b48
AbakusW <camix1984@gmail.com>
2021-01-29 11:00:06 +0100

    removal of Liqo Agent components and dependencies

--- internal/tray-agent/agent/client/doc.go
+++
@@ -1,5 +0,0 @@
-/*
-package client contains the data types and functions Liqo Agent exploits
-to perform cluster operations.
-*/
-package client
--- internal/tray-agent/agent/logic/doc.go
+++
@@ -1,7 +0,0 @@
-/*
-Package logic provides the functions implementing Liqo Tray Agent capabilities.
-
-OnReady() and OnExit() are the two callback wrappers passed to the app_indicator/Run() function that acts as runtime
-controller of the tray agent.
-*/
-package logic
