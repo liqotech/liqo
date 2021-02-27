852d1f92e0039eba46bdf6979a94d234a8cd42d8
alacuku <aldo.lacu@live.it>
2021-02-27 10:14:13 +0100

    fixing permissions on liqo-gateway for the clusterconfigs resources adding CRD validations for the network configuration in the clusterconfig CRD

--- apis/config/v1alpha1/clusterconfig_types.go
+++ apis/config/v1alpha1/clusterconfig_types.go
@@ -139,10 +139,14 @@ type LiqonetConfig struct {
 	//Add here the subnets already used in your environment as a list in CIDR notation (e.g. [10.1.0.0/16, 10.200.1.0/24]).
 	ReservedSubnets []string `json:"reservedSubnets"`
 	//the subnet used by the cluster for the pods, in CIDR notation
+	//at this moment only /16 subnets are supported
+	// +kubebuilder:validation:Pattern="^([0-9]{1,3}.){3}[0-9]{1,3}(/(16))"
 	PodCIDR string `json:"podCIDR"`
 	//the subnet used by the cluster for the services, in CIDR notation
+	// +kubebuilder:validation:Pattern="^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))$"
 	ServiceCIDR string `json:"serviceCIDR"`
-	//set this flag to true if you are using GKE with kubenet CNI
+	//set this flag to true if you are using GKE, default value is "false"
+	// +kubebuilder:default=false
 	GKEProvider bool `json:"GKEProvider"`
 }
 
