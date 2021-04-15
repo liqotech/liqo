d7cf82fdd0daccf06d954fbb23bc2f7128a61c5f
Andreagit97 <andreaterzolo3@gmail.com>
2021-04-15 15:44:11 +0200

    Add NamespaceMap CRD to liqo's APIs
    This commit adds scaffolded files for NamespaceMap resource, Group is 'virtualkubelet.liqo.io' and Version is 'v1alpha1'

---
+++ apis/virtualKubelet/v1alpha1/namespacemap_types.go
@@ -0,0 +1,65 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NamespaceMapSpec defines the desired state of NamespaceMap
+type NamespaceMapSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+}
+
+// NamespaceMapStatus defines the observed state of NamespaceMap
+type NamespaceMapStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// NattingTable is used to monitor remote Namespaces creation, it allows virtualKubelet to know how many local namespaces
+	// are offloaded on the remote cluster associated to this NamespaceMap. key = local namespace name , value = remote namespace name
+	NattingTable map[string]string `json:"nattingTable,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// NamespaceMap is the Schema for the namespacemaps API
+type NamespaceMap struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NamespaceMapSpec   `json:"spec,omitempty"`
+	Status NamespaceMapStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// NamespaceMapList contains a list of NamespaceMap
+type NamespaceMapList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NamespaceMap `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NamespaceMap{}, &NamespaceMapList{})
+}
