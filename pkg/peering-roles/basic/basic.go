85798c3e9df4a2b76139c07ef21fd61243ae77d0
Alessandro Olivero <aleoli96@gmail.com>
2021-04-22 15:21:06 +0200

    remote peering rbac

---
+++ pkg/peering-roles/basic/basic.go
@@ -0,0 +1,4 @@
+package basic
+
+// +kubebuilder:rbac:groups=discovery.liqo.io,resources=resourcerequests,verbs=get;update;patch;list;watch;delete
+// +kubebuilder:rbac:groups=discovery.liqo.io,resources=resourcerequests/status,verbs=get;update;patch;list;watch;delete
