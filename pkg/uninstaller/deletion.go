d02a9a9920b19264a9c904e5d7a2524d0829289f
Alex Palesandro <alex.palesandro@gmail.com>
2021-04-25 00:05:36 +0200

    Add wait condition to Unistaller pre-delete hook
    Pre-delete hook now waits that all peering are correctly deactivated before exiting. This prevent the possibility of having resources remaining on the cluster after uninstall.
    
    Co-authored-by: Marco Iorio <marco.iorio@polito.it>

---
+++ pkg/uninstaller/deletion.go
@@ -0,0 +1,53 @@
+package uninstaller
+
+import (
+	"context"
+	clusterconfigV1alpha1 "github.com/liqotech/liqo/apis/config/v1alpha1"
+	discoveryV1alpha1 "github.com/liqotech/liqo/apis/discovery/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/klog/v2"
+)
+
+// UnjoinClusters disables outgoing peerings with available clusters
+func UnjoinClusters(client dynamic.Interface) error {
+	foreign, err := getForeignList(client)
+	if err != nil {
+		return err
+	}
+	klog.Infof("Unjoin %v ForeignClusters", len(foreign.Items))
+	r1 := client.Resource(discoveryV1alpha1.ForeignClusterGroupVersionResource)
+	for _, item := range foreign.Items {
+		patch := []byte(`{"spec": {"join": false}}`)
+		_, err = r1.Namespace(item.Namespace).Patch(context.TODO(), item.Name, types.MergePatchType, patch, metav1.PatchOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// DisableBroadcasting disables broadcasting of advertisements from the ClusterConfig
+func DisableBroadcasting(client dynamic.Interface) error {
+	r1 := client.Resource(clusterconfigV1alpha1.ClusterConfigGroupVersionResource)
+	t, err := r1.Namespace("").List(context.TODO(), metav1.ListOptions{TypeMeta: metav1.TypeMeta{}})
+	if err != nil {
+		return err
+	}
+	klog.V(5).Infof("Getting clusterConfigs")
+	var clusterconfigs clusterconfigV1alpha1.ClusterConfigList
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(t.UnstructuredContent(), &clusterconfigs); err != nil {
+		return err
+	}
+	klog.V(5).Infof("Patching ClusterConfigs")
+	for _, item := range clusterconfigs.Items {
+		patch := []byte(`{"spec": {"advertisementConfig": { "outgoingConfig" : { "enableBroadcaster" : false}}}}`)
+		_, err = r1.Namespace(item.Namespace).Patch(context.TODO(), item.Name, types.MergePatchType, patch, metav1.PatchOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ pkg/uninstaller/wait.go
@@ -0,0 +1,135 @@
+package uninstaller
+
+import (
+	"context"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/klog/v2"
+	"sync"
+	"time"
+)
+
+// WaitForResources waits until existing peerings are disabled and associated resources are removed.
+func WaitForResources(client dynamic.Interface) error {
+	klog.Info("Waiting for Liqo Resources to be correctly deleted")
+	var wg sync.WaitGroup
+	deletionResult := make(chan *resultType, len(toCheck))
+	conditionResult := make(chan *resultType, ConditionsToCheck)
+	wg.Add(len(toCheck) + ConditionsToCheck)
+	for _, resource := range toCheck {
+		go WaitForEffectiveDeletion(client, resource, deletionResult, &wg, CheckDeletion)
+	}
+	go WaitForEffectiveDeletion(client, toCheckDeleted{}, conditionResult, &wg, CheckUnjoin)
+	wg.Wait()
+
+	close(deletionResult)
+	close(conditionResult)
+
+	for elem := range deletionResult {
+		if !elem.Success {
+			klog.Errorf("Error while waiting for %s to be deleted", elem.Resource.gvr.GroupResource())
+			return nil
+		}
+		printLabels, _ := generateLabelString(elem.Resource.labelSelector)
+		klog.Infof("%s instances with \"%s\" labels correctly deleted", elem.Resource.gvr.GroupResource(), printLabels)
+	}
+	for elem := range conditionResult {
+		if !elem.Success {
+			klog.Errorf("All peerings have been correctly deleted")
+			return nil
+		}
+
+	}
+
+	return nil
+}
+
+// WaitForEffectiveDeletion waits until toCheck resources are deleted.
+func WaitForEffectiveDeletion(client dynamic.Interface, toCheck toCheckDeleted, result chan *resultType, wg *sync.WaitGroup, funcCheck func(client dynamic.Interface, res *resultType, quit chan struct{}, toCheck toCheckDeleted)) {
+	defer wg.Done()
+	ticker := time.NewTicker(TickerInterval)
+	timeout := time.NewTicker(TickerTimeout)
+	quit := make(chan struct{})
+	var res = &resultType{
+		Resource: toCheck,
+		Success:  false,
+	}
+	for {
+		select {
+		case <-timeout.C:
+			close(quit)
+		case <-ticker.C:
+			funcCheck(client, res, quit, toCheck)
+		case <-quit:
+			ticker.Stop()
+			timeout.Stop()
+			result <- res
+			return
+		}
+	}
+}
+
+func CheckDeletion(client dynamic.Interface, res *resultType, quit chan struct{}, toCheck toCheckDeleted) {
+	value, wError := CheckObjectsDeletion(client, toCheck)
+	if value {
+		res.Success = true
+		close(quit)
+	}
+	if wError != nil {
+		klog.Infof("Error while waiting for deletion of resource %s: %s", toCheck.gvr.GroupResource(), wError.Error())
+		close(quit)
+	}
+	printLabels, _ := generateLabelString(toCheck.labelSelector)
+	klog.Infof("Waiting for %s instances with %s labels to be correctly deleted", toCheck.gvr.GroupResource(), printLabels)
+}
+
+func CheckUnjoin(client dynamic.Interface, res *resultType, quit chan struct{}, toCheck toCheckDeleted) {
+	foreignClusterList, err := getForeignList(client)
+	if err != nil {
+		close(quit)
+	}
+
+	flag := checkPeeringsStatus(foreignClusterList)
+	if flag {
+		klog.Infof("All incoming or outgoing peering are disabled")
+		res.Success = true
+		close(quit)
+	}
+}
+
+// CheckObjectsDeletion verifies that objects of a certain type have been deleted or are not present on the server.
+// It returns true when this last condition is verified.
+func CheckObjectsDeletion(client dynamic.Interface, objectsToCheck toCheckDeleted) (bool, error) {
+	var (
+		objectList  *unstructured.UnstructuredList
+		err         error
+		labelString string
+	)
+
+	if labelString, err = generateLabelString(objectsToCheck.labelSelector); err != nil {
+		return false, err
+	}
+	options := metav1.ListOptions{
+		LabelSelector: labelString,
+	}
+
+	objectList, err = client.Resource(objectsToCheck.gvr).Namespace("").List(context.TODO(), options)
+
+	if apierrors.IsNotFound(err) {
+		return true, nil
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	if len(objectList.Items) == 0 {
+		klog.V(6).Infof("%s not found", objectsToCheck.gvr)
+		return true, nil
+	}
+
+	return false, nil
+
+}
