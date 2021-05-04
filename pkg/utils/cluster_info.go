d4e142b45064ae4315b1a6e41c5069e02f7d921f
Andreagit97 <andreaterzolo3@gmail.com>
2021-05-04 11:55:18 +0200

    Change the way in which ConfigMap is obtained
    Instead of using "List", now it's used "Get" to obtain directly the correct ConfigMap.

--- pkg/utils/cluster_info.go
+++ pkg/utils/cluster_info.go
@@ -2,33 +2,26 @@ package utils
 
 import (
 	"context"
-	"fmt"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
-	clusterIDConfMap = "cluster-id"
-	// Todo: Is there a place where i can get this?
+	clusterIDConfMap   = "cluster-id"
 	configMapNamespace = "liqo"
 )
 
 func GetClusterID(c client.Client) (string, error) {
 
-	configMaps := &corev1.ConfigMapList{}
-	// possibility to specify "Liqo" namespace if it is possible to get it somewhere
-	if err := c.List(context.TODO(), configMaps, client.InNamespace(configMapNamespace)); err != nil {
-		klog.Error(err, "unable to get ConfigMapList")
-		return "", err
-	}
-	if len(configMaps.Items) != 1 {
-		err := fmt.Errorf("there are %d ConfigMaps at the moment, unstable condition", len(configMaps.Items))
-		klog.Error(err)
+	configMap := &corev1.ConfigMap{}
+	if err := c.Get(context.TODO(), types.NamespacedName{Namespace: configMapNamespace, Name: clusterIDConfMap}, configMap); err != nil {
+		klog.Errorf("%s, unable to get ConfigMap '%s' in namespace '%s'", err, clusterIDConfMap, configMapNamespace)
 		return "", err
 	}
 
-	clusterID := configMaps.Items[0].Data[clusterIDConfMap]
+	clusterID := configMap.Data[clusterIDConfMap]
 	klog.Infof("ClusterID is '%s'", clusterID)
 	return clusterID, nil
 }
