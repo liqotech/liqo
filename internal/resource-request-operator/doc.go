328ed238691ad4ad3be48e9c4ab27d2b87be7cec
giuseppe_alicino <s265373@studenti.polito.it>
2021-05-10 17:23:23 +0200

    Fixed all files linting issues.

---
+++ internal/resource-request-operator/doc.go
@@ -0,0 +1,5 @@
+/*Package resourcerequestoperator contains the ResourceRequest controller
+which reconciles the resource and creates new ResourceOffer and related tests.
+utils.go contains all utility function.
+*/
+package resourcerequestoperator
--- internal/resource-request-operator/resourceRequest_controller_test.go
+++ internal/resource-request-operator/resourceRequest_controller_test.go
@@ -1,4 +1,4 @@
-package resourceRequestOperator
+package resourcerequestoperator
 
 import (
 	"context"
