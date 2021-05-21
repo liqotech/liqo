c0e5b894f41c91c489af9fb8f0d86b2b7c59e310
Alessandro Olivero <aleoli96@gmail.com>
2021-05-21 12:40:16 +0000

    add virtual-kubelet local ClusterRole

---
+++ pkg/virtualKubelet/roles/role.go
@@ -0,0 +1,16 @@
+// Package roles defines the ClusterRole containing the permissions required by the virtual kubelet in the local cluster.
+package roles
+
+// +kubebuilder:rbac:groups="",resources=configmaps;services;namespaces;secrets,verbs=get;list;watch;delete;create
+// +kubebuilder:rbac:groups="",resources=nodes,verbs=get;update;list;watch;delete;create
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;patch;list;watch;delete;create
+// +kubebuilder:rbac:groups="",resources=pods/status;services/status;namespaces/status;nodes/status,verbs=get;update;patch;list;watch;delete;create
+
+// +kubebuilder:rbac:groups=apps,resources=replicasets,verbs=get;list;watch
+// +kubebuilder:rbac:groups=discovery.k8s.io,resources=endpointslices,verbs=create;get;list;watch
+
+// +kubebuilder:rbac:groups=certificates.k8s.io,resources=certificatesigningrequests,verbs=create;get;list;watch
+
+// +kubebuilder:rbac:groups=virtualkubelet.liqo.io,resources=namespacenattingtables,verbs=get;update;patch;list;watch;delete;create
+// +kubebuilder:rbac:groups=net.liqo.io,resources=tunnelendpoints,verbs=get;list;watch
+// +kubebuilder:rbac:groups=sharing.liqo.io,resources=advertisements,verbs=get;list;watch;update;delete
