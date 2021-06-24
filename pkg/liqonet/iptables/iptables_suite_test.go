4bfbff16dda413ce77b95adc3c8fcd58b38e7624
davidefalcone1 <davidefalcone1@gmail.com>
2021-06-24 13:15:51 +0000

    Minor fixes on IPTables module tests
    This minor PR removes functions used to create resources during IPTables module tests.

--- pkg/liqonet/iptables/iptables_suite_test.go
+++ pkg/liqonet/iptables/iptables_suite_test.go
@@ -3,6 +3,7 @@ package iptables
 import (
 	"testing"
 
+	. "github.com/coreos/go-iptables/iptables"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -11,3 +12,18 @@ func TestIptables(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Iptables Suite")
 }
+
+var _ = BeforeSuite(func() {
+	var err error
+	h, err = NewIPTHandler()
+	Expect(err).To(BeNil())
+	ipt, err = New()
+	Expect(err).To(BeNil())
+	err = h.Init()
+	Expect(err).To(BeNil())
+})
+
+var _ = AfterSuite(func() {
+	err := h.Terminate()
+	Expect(err).To(BeNil())
+})
