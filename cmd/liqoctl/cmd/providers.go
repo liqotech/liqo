d64b2630106a46d10fc4cd2662620895ab8a81ae
Alessandro Olivero <aleoli96@gmail.com>
2021-08-16 15:32:32 +0000

    add liqoctl k3s provider

--- cmd/liqoctl/cmd/providers.go
+++ cmd/liqoctl/cmd/providers.go
@@ -6,13 +6,15 @@ import (
 	"github.com/liqotech/liqo/pkg/liqoctl/install/aks"
 	"github.com/liqotech/liqo/pkg/liqoctl/install/eks"
 	"github.com/liqotech/liqo/pkg/liqoctl/install/gke"
+	"github.com/liqotech/liqo/pkg/liqoctl/install/k3s"
 	"github.com/liqotech/liqo/pkg/liqoctl/install/kubeadm"
 )
 
-var providers = []string{"kubeadm", "eks", "gke", "aks"}
+var providers = []string{"kubeadm", "k3s", "eks", "gke", "aks"}
 
 var providerInitFunc = map[string]func(*flag.FlagSet){
 	"kubeadm": kubeadm.GenerateFlags,
+	"k3s":     k3s.GenerateFlags,
 	"eks":     eks.GenerateFlags,
 	"gke":     gke.GenerateFlags,
 	"aks":     aks.GenerateFlags,
