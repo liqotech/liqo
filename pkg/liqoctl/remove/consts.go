b3c5f37c1e8b6f32573f6e10c549bfc7dea0df48
Alessandro Olivero <aleoli96@gmail.com>
2021-09-10 14:59:09 +0000

    new liqoctl remove command

---
+++ pkg/liqoctl/remove/consts.go
@@ -0,0 +1,42 @@
+// Copyright 2019-2021 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package remove
+
+const (
+	// LiqoctlRemoveShortHelp contains the short help string for liqoctl remove command.
+	LiqoctlRemoveShortHelp = "Disable a peering with a remote cluster"
+	// LiqoctlRemoveLongHelp contains the Long help string for liqoctl remove command.
+	LiqoctlRemoveLongHelp = `Disable a peering with a remote cluster
+
+$ liqoctl remove cluster my-cluster
+
+`
+	// UseCommand contains the verb of the remove command.
+	UseCommand = "remove"
+	// ClusterResourceName contains the name of the resource removed in liqoctl remove.
+	ClusterResourceName = "cluster"
+	// SuccessfulMessage is printed when a remove cluster command has scucceded.
+	SuccessfulMessage = `
+	Success 👌! You have correctly removed the cluster %s and disabled an outgoing peering towards it.
+You can now:
+
+* Check the status of the peering to see when it is completely disabled. 
+The field OutgoingPeering of the foreigncluster should be in "None":
+
+kubectl get foreignclusters %s
+
+* For more information about Liqo have a look to: https://doc.liqo.io
+`
+)
---
+++ pkg/liqoctl/remove/handler.go
@@ -0,0 +1,84 @@
+// Copyright 2019-2021 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package remove
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	discoveryv1alpha1 "github.com/liqotech/liqo/apis/discovery/v1alpha1"
+	"github.com/liqotech/liqo/pkg/liqoctl/common"
+	foreigncluster "github.com/liqotech/liqo/pkg/utils/foreignCluster"
+)
+
+// ClusterArgs encapsulates arguments required to disable an outgoing peering to a remote cluster.
+type ClusterArgs struct {
+	ClusterName string
+	ClusterID   string
+}
+
+// HandleRemoveCommand handles the remove command, configuring all the resources required to disable an outgoing peering.
+func HandleRemoveCommand(ctx context.Context, t *ClusterArgs) {
+	restConfig := common.GetLiqoctlRestConfOrDie()
+
+	k8sClient, err := client.New(restConfig, client.Options{})
+	if err != nil {
+		klog.Fatalf(err.Error())
+	}
+
+	if err := processRemoveCluster(ctx, t, k8sClient); err != nil {
+		klog.Fatalf(err.Error())
+	}
+
+	err = printSuccessfulOutputMessage(ctx, t, k8sClient)
+	if err != nil {
+		klog.Fatalf(err.Error())
+	}
+}
+
+func printSuccessfulOutputMessage(ctx context.Context, t *ClusterArgs, k8sClient client.Client) error {
+	fc, err := foreigncluster.GetForeignClusterByID(ctx, k8sClient, t.ClusterID)
+	if err != nil {
+		return err
+	}
+	fmt.Printf(SuccessfulMessage, t.ClusterName, fc.Name)
+	return nil
+}
+
+func processRemoveCluster(ctx context.Context, t *ClusterArgs, k8sClient client.Client) error {
+	err := enforceForeignCluster(ctx, k8sClient, t)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func enforceForeignCluster(ctx context.Context, cl client.Client, t *ClusterArgs) error {
+	// Get ForeignCluster
+	var foreignCluster discoveryv1alpha1.ForeignCluster
+	if err := cl.Get(ctx, types.NamespacedName{Name: t.ClusterName}, &foreignCluster); err != nil {
+		return err
+	}
+
+	t.ClusterID = foreignCluster.Spec.ClusterIdentity.ClusterID
+
+	// Update ForeignCluster
+	foreignCluster.Spec.OutgoingPeeringEnabled = discoveryv1alpha1.PeeringEnabledNo
+	return cl.Update(ctx, &foreignCluster)
+}
