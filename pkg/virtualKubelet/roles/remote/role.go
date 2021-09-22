63e4df4336d130958ddb5b51fa2b7314ec26e804
Alessandro Olivero <aleoli96@gmail.com>
2021-09-22 12:20:02 +0000

    fix metrics permission

--- pkg/virtualKubelet/roles/remote/role.go
+++ pkg/virtualKubelet/roles/remote/role.go
@@ -17,6 +17,7 @@ package remote
 
 // +kubebuilder:rbac:groups="",resources=configmaps;services;secrets;pods,verbs=get;list;watch;update;patch;delete;create
 // +kubebuilder:rbac:groups="",resources=pods/status;services/status,verbs=get;update;patch;list;watch;delete;create
+// +kubebuilder:rbac:groups=metrics.k8s.io,resources=pods,verbs=get;list;watch
 
 // +kubebuilder:rbac:groups=apps,resources=replicasets,verbs=get;list;watch;update;create;delete
 // +kubebuilder:rbac:groups=discovery.k8s.io,resources=endpointslices,verbs=get;list;watch;create;update;patch;delete
