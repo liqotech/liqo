liqotech/liqo

commit 7c6785ab7b95b9e58caefff1a1d463e23adc768f

Remove the cluster config from network operators

apis/config/v1alpha1/zz_generated.deepcopy.go
@@ -139,7 +139,6 @@ func (in *ClusterConfigSpec) DeepCopyInto(out *ClusterConfigSpec) {
 	*out = *in
 	in.AdvertisementConfig.DeepCopyInto(&out.AdvertisementConfig)
 	in.DiscoveryConfig.DeepCopyInto(&out.DiscoveryConfig)
-	in.LiqonetConfig.DeepCopyInto(&out.LiqonetConfig)
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ClusterConfigSpec.
@@ -200,31 +199,6 @@ func (in *LabelPolicy) DeepCopy() *LabelPolicy {
 		return nil
 	}
 	out := new(LabelPolicy)
-	in.DeepCopyInto(out)
-	return out
-}
-
-// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
-func (in *LiqonetConfig) DeepCopyInto(out *LiqonetConfig) {
-	*out = *in
-	if in.ReservedSubnets != nil {
-		in, out := &in.ReservedSubnets, &out.ReservedSubnets
-		*out = make([]CIDR, len(*in))
-		copy(*out, *in)
-	}
-	if in.AdditionalPools != nil {
-		in, out := &in.AdditionalPools, &out.AdditionalPools
-		*out = make([]CIDR, len(*in))
-		copy(*out, *in)
-	}
-}
-
-// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new LiqonetConfig.
-func (in *LiqonetConfig) DeepCopy() *LiqonetConfig {
-	if in == nil {
-		return nil
-	}
-	out := new(LiqonetConfig)
 	in.DeepCopyInto(out)
 	return out
 }

apis/config/v1alpha1/clusterconfig_types.go
@@ -29,11 +29,7 @@ type ClusterConfigSpec struct {
 	// AdvertisementConfig defines the configuration for the advertisement protocol.
 	AdvertisementConfig AdvertisementConfig `json:"resourceSharingConfig"`
 	DiscoveryConfig     DiscoveryConfig     `json:"discoveryConfig"`
-	LiqonetConfig       LiqonetConfig       `json:"liqonetConfig"`
 }
-
-// +kubebuilder:validation:Pattern="^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))$"
-type CIDR string
 
 // AdvertisementConfig defines the configuration for the advertisement protocol.
 type AdvertisementConfig struct {
@@ -133,24 +129,6 @@ type DiscoveryConfig struct {
 
 	AuthServiceAddress string `json:"authServiceAddress,omitempty"`
 	AuthServicePort    string `json:"authServicePort,omitempty"`
-}
-
-// LiqonetConfig defines the configuration of the Liqo Networking.
-type LiqonetConfig struct {
-	// This field is used by the IPAM embedded in the tunnelEndpointCreator.
-	// Subnets listed in this field are excluded from the list of possible subnets used for natting POD CIDR.
-	// Add here the subnets already used in your environment as a list in CIDR notation
-	// (e.g. [10.1.0.0/16, 10.200.1.0/24]).
-	ReservedSubnets []CIDR `json:"reservedSubnets"`
-	// The subnet used by the cluster for the pods, in CIDR notation
-	// +kubebuilder:validation:Pattern="^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))$"
-	PodCIDR string `json:"podCIDR"`
-	// The subnet used by the cluster for the services, in CIDR notation
-	// +kubebuilder:validation:Pattern="^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))$"
-	ServiceCIDR string `json:"serviceCIDR"`
-	// Set of additional user-defined network pools.
-	// Default set of network pools is: [192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12]
-	AdditionalPools []CIDR `json:"additionalPools"`
 }
 
 // ClusterConfigStatus defines the observed state of ClusterConfig.

cmd/liqonet/endpoint-creator-operator.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
+	"regexp"
 	"sync"
 	"time"
 
@@ -10,15 +13,63 @@ import (
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	clusterConfig "github.com/liqotech/liqo/apis/config/v1alpha1"
 	"github.com/liqotech/liqo/internal/liqonet/tunnelEndpointCreator"
 	liqoconst "github.com/liqotech/liqo/pkg/consts"
 	liqonetIpam "github.com/liqotech/liqo/pkg/liqonet/ipam"
 	"github.com/liqotech/liqo/pkg/liqonet/utils"
 	"github.com/liqotech/liqo/pkg/mapperUtils"
+	"github.com/liqotech/liqo/pkg/utils/args"
 )
 
-func runEndpointCreatorOperator(commonFlags *liqonetCommonFlags) {
+type networkManagerFlags struct {
+	podCIDR     string
+	serviceCIDR string
+
+	additionalPools args.StringList
+	reservedPools   args.StringList
+}
+
+func addNetworkManagerFlags(managerFlags *networkManagerFlags) {
+	flag.StringVar(&managerFlags.podCIDR, "manager.pod-cidr", "", "The subnet used by the cluster for the pods, in CIDR notation")
+	flag.StringVar(&managerFlags.serviceCIDR, "manager.service-cidr", "", "The subnet used by the cluster for the pods, in services notation")
+	flag.Var(&managerFlags.reservedPools, "manager.reserved-pools",
+		"Private CIDRs slices used by the Kubernetes infrastructure, in addition to the pod and service CIDR (e.g., the node subnet).")
+	flag.Var(&managerFlags.additionalPools, "manager.additional-pools",
+		"Network pools used to map a cluster network into another one in order to prevent conflicts, in addition to standard private CIDRs.")
+}
+
+func validateNetworkManagerFlags(managerFlags *networkManagerFlags) error {
+	cidrRegex := regexp.MustCompile(`^(\d{1,3}.){3}\d{1,3}(/(\d|[12]\d|3[012]))$`)
+
+	if !cidrRegex.MatchString(managerFlags.podCIDR) {
+		return fmt.Errorf("pod CIDR is empty or invalid (%q)", managerFlags.podCIDR)
+	}
+
+	if !cidrRegex.MatchString(managerFlags.serviceCIDR) {
+		return fmt.Errorf("service CIDR is empty or invalid (%q)", managerFlags.serviceCIDR)
+	}
+
+	for _, pool := range managerFlags.reservedPools.StringList {
+		if !cidrRegex.MatchString(pool) {
+			return fmt.Errorf("reserved pool entry empty or invalid (%q)", pool)
+		}
+	}
+
+	for _, pool := range managerFlags.additionalPools.StringList {
+		if !cidrRegex.MatchString(pool) {
+			return fmt.Errorf("additional pool entry empty or invalid (%q)", pool)
+		}
+	}
+
+	return nil
+}
+
+func runEndpointCreatorOperator(commonFlags *liqonetCommonFlags, managerFlags *networkManagerFlags) {
+	if err := validateNetworkManagerFlags(managerFlags); err != nil {
+		klog.Errorf("Failed to parse flags: %s", err)
+		os.Exit(1)
+	}
+
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		MapperProvider:     mapperUtils.LiqoMapperProvider(scheme),
 		Scheme:             scheme,
@@ -30,11 +81,19 @@ func runEndpointCreatorOperator(commonFlags *liqonetCommonFlags) {
 	}
 	clientset := kubernetes.NewForConfigOrDie(mgr.GetConfig())
 	dynClient := dynamic.NewForConfigOrDie(mgr.GetConfig())
-	ipam := liqonetIpam.NewIPAM()
-	err = ipam.Init(liqonetIpam.Pools, dynClient, liqoconst.NetworkManagerIpamPort)
+
+	ipam, err := initializeIPAM(dynClient, managerFlags)
 	if err != nil {
-		klog.Errorf("cannot init IPAM:%w", err)
+		klog.Errorf("Failed to initialize IPAM: %w", err)
+		os.Exit(1)
 	}
+
+	externalCIDR, err := ipam.GetExternalCIDR(utils.GetMask(managerFlags.podCIDR))
+	if err != nil {
+		klog.Errorf("Failed to initialize the external CIDR: %w", err)
+		os.Exit(1)
+	}
+
 	podNamespace, err := utils.GetPodNamespace()
 	if err != nil {
 		klog.Errorf("unable to get pod namespace: %v", err)
@@ -48,32 +107,59 @@ func runEndpointCreatorOperator(commonFlags *liqonetCommonFlags) {
 		Manager:                    mgr,
 		Namespace:                  podNamespace,
 		WaitConfig:                 &sync.WaitGroup{},
-		ReservedSubnets:            make([]string, 0),
-		AdditionalPools:            make([]string, 0),
 		Configured:                 make(chan bool, 1),
 		ForeignClusterStartWatcher: make(chan bool, 1),
 		ForeignClusterStopWatcher:  make(chan struct{}),
 		IPManager:                  ipam,
 		RetryTimeout:               30 * time.Second,
+
+		PodCIDR:      managerFlags.podCIDR,
+		ExternalCIDR: externalCIDR,
 	}
-	r.WaitConfig.Add(3)
-	// starting configuration watcher
-	config, err := ctrl.GetConfig()
-	if err != nil {
-		klog.Error(err)
-		os.Exit(2)
-	}
-	r.WatchConfiguration(config, &clusterConfig.GroupVersion)
+
+	r.WaitConfig.Add(2)
+
 	if err = r.SetupWithManager(mgr); err != nil {
 		klog.Errorf("unable to create controller controller TunnelEndpointCreator: %s", err)
 		os.Exit(1)
 	}
+
 	go r.StartForeignClusterWatcher()
 	go r.StartServiceWatcher()
 	go r.StartSecretWatcher()
+
 	klog.Info("starting manager as tunnelEndpointCreator-operator")
 	if err := mgr.Start(r.SetupSignalHandlerForTunEndCreator()); err != nil {
 		klog.Errorf("an error occurred while starting manager: %s", err)
 		os.Exit(1)
 	}
+}
+
+func initializeIPAM(client dynamic.Interface, managerFlags *networkManagerFlags) (*liqonetIpam.IPAM, error) {
+	ipam := liqonetIpam.NewIPAM()
+
+	if err := ipam.Init(liqonetIpam.Pools, client, liqoconst.NetworkManagerIpamPort); err != nil {
+		return nil, err
+	}
+
+	if err := ipam.SetPodCIDR(managerFlags.podCIDR); err != nil {
+		return nil, err
+	}
+	if err := ipam.SetServiceCIDR(managerFlags.serviceCIDR); err != nil {
+		return nil, err
+	}
+
+	for _, pool := range managerFlags.additionalPools.StringList {
+		if err := ipam.AddNetworkPool(pool); err != nil {
+			return nil, err
+		}
+	}
+
+	for _, pool := range managerFlags.reservedPools.StringList {
+		if err := ipam.AcquireReservedSubnet(pool); err != nil {
+			return nil, err
+		}
+	}
+
+	return ipam, nil
 }

cmd/liqonet/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	liqoconst "github.com/liqotech/liqo/pkg/consts"
 )
@@ -14,5 +15,6 @@ type liqonetCommonFlags struct {
 func addCommonFlags(liqonet *liqonetCommonFlags) {
 	flag.StringVar(&liqonet.metricsAddr, "metrics-bind-addr", ":0", "The address the metric endpoint binds to.")
 	flag.StringVar(&liqonet.runAs, "run-as", liqoconst.LiqoGatewayOperatorName,
-		"The accepted values are: liqo-gateway, liqo-route, tunnelEndpointCreator-operator. The default value is \"liqo-gateway\"")
+		fmt.Sprintf("The accepted values are: %q, %q, %q.",
+			liqoconst.LiqoGatewayOperatorName, liqoconst.LiqoRouteOperatorName, liqoconst.LiqoNetworkManagerName))
 }

cmd/liqonet/main.go
@@ -47,12 +47,17 @@ func init() {
 
 func main() {
 	klog.InitFlags(nil)
+
 	commonFlags := &liqonetCommonFlags{}
 	routeFlags := &routeOperatorFlags{}
 	gatewayFlags := &gatewayOperatorFlags{}
+	managerFlags := &networkManagerFlags{}
+
 	addCommonFlags(commonFlags)
 	addGatewayOperatorFlags(gatewayFlags)
 	addRouteOperatorFlags(routeFlags)
+	addNetworkManagerFlags(managerFlags)
+
 	flag.Parse()
 
 	switch commonFlags.runAs {
@@ -61,6 +66,6 @@ func main() {
 	case liqoconst.LiqoGatewayOperatorName:
 		runGatewayOperator(commonFlags, gatewayFlags)
 	case liqoconst.LiqoNetworkManagerName:
-		runEndpointCreatorOperator(commonFlags)
+		runEndpointCreatorOperator(commonFlags, managerFlags)
 	}
 }

deployments/liqo/crds/config.liqo.io_clusterconfigs.yaml
@@ -91,42 +91,6 @@ spec:
                 - service
                 - ttl
                 type: object
-              liqonetConfig:
-                description: LiqonetConfig defines the configuration of the Liqo Networking.
-                properties:
-                  additionalPools:
-                    description: 'Set of additional user-defined network pools. Default
-                      set of network pools is: [192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12]'
-                    items:
-                      pattern: ^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))$
-                      type: string
-                    type: array
-                  podCIDR:
-                    description: The subnet used by the cluster for the pods, in CIDR
-                      notation
-                    pattern: ^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))$
-                    type: string
-                  reservedSubnets:
-                    description: This field is used by the IPAM embedded in the tunnelEndpointCreator.
-                      Subnets listed in this field are excluded from the list of possible
-                      subnets used for natting POD CIDR. Add here the subnets already
-                      used in your environment as a list in CIDR notation (e.g. [10.1.0.0/16,
-                      10.200.1.0/24]).
-                    items:
-                      pattern: ^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))$
-                      type: string
-                    type: array
-                  serviceCIDR:
-                    description: The subnet used by the cluster for the services,
-                      in CIDR notation
-                    pattern: ^([0-9]{1,3}.){3}[0-9]{1,3}(/([0-9]|[1-2][0-9]|3[0-2]))$
-                    type: string
-                required:
-                - additionalPools
-                - podCIDR
-                - reservedSubnets
-                - serviceCIDR
-                type: object
               resourceSharingConfig:
                 description: AdvertisementConfig defines the configuration for the
                   advertisement protocol.
@@ -219,7 +183,6 @@ spec:
                 type: object
             required:
             - discoveryConfig
-            - liqonetConfig
             - resourceSharingConfig
             type: object
           status:

deployments/liqo/templates/clusterconfig.yaml
@@ -30,5 +30,3 @@ spec:
     {{- else if .Values.auth.ingress.enable }}
     authServicePort: "443"
     {{- end }}
-  liqonetConfig:
-    {{- .Values.networkManager.config | toYaml | nindent 4 }}

deployments/liqo/templates/liqo-network-manager-deployment.yaml
@@ -33,7 +33,17 @@ spec:
             - name: ipam-api
               containerPort: 6000
           args:
-            - --run-as=tunnelEndpointCreator-operator
+            - --run-as=liqo-network-manager
+            - --manager.pod-cidr={{ .Values.networkManager.config.podCIDR }}
+            - --manager.service-cidr={{ .Values.networkManager.config.serviceCIDR }}
+            {{- if .Values.networkManager.config.reservedSubnets }}
+            {{- $d := dict "commandName" "--manager.reserved-pools" "list" .Values.networkManager.config.reservedSubnets }}
+            {{- include "liqo.concatenateList" $d | nindent 12 }}
+            {{- end }}
+            {{- if .Values.networkManager.config.additionalPools }}
+            {{- $d := dict "commandName" "--manager.additional-pools" "list" .Values.networkManager.config.additionalPools }}
+            {{- include "liqo.concatenateList" $d | nindent 12 }}
+            {{- end }}
             {{- if .Values.networkManager.pod.extraArgs }}
             {{- toYaml .Values.networkManager.pod.extraArgs | nindent 12 }}
             {{- end }}

commit 3a34ea22166667a5bb0f91dab4e1818cf5685281

fix (network-manager): correct a bug (ref #912) which prevented the network manager to restart
Subnets in CIDR notation can be passed to the network manager as parameters
in order to prevent them to be used for remote clusters. A bug prevented
the network manager to restart when networks to be reserved are passed as
arguments.  The bug introduced in PR #868 has been fixed. If a subnet has to be
removed from the reserved networks we just remove the parameter from the network
manager and restart the operator.

apis/net/v1alpha1/ipamstorage_types.go
@@ -55,6 +55,8 @@ type IpamSpec struct {
 	Prefixes map[string][]byte `json:"prefixes"`
 	// Network pools.
 	Pools []string `json:"pools"`
+	// Reserved Networks. Subnets listed in this field are excluded from the list of possible subnets used for natting POD CIDR.
+	ReservedSubnets []string `json:"reservedSubnets"`
 	// Map used to keep track of networks assigned to clusters. Key is the remote cluster ID, value is a the set of
 	// networks used by the remote cluster.
 	ClusterSubnets map[string]Subnets `json:"clusterSubnets"`

apis/net/v1alpha1/zz_generated.deepcopy.go
@@ -119,6 +119,11 @@ func (in *IpamSpec) DeepCopyInto(out *IpamSpec) {
 		*out = make([]string, len(*in))
 		copy(*out, *in)
 	}
+	if in.ReservedSubnets != nil {
+		in, out := &in.ReservedSubnets, &out.ReservedSubnets
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	if in.ClusterSubnets != nil {
 		in, out := &in.ClusterSubnets, &out.ClusterSubnets
 		*out = make(map[string]Subnets, len(*in))

cmd/liqonet/network-manager.go
@@ -55,7 +55,8 @@ func addNetworkManagerFlags(managerFlags *networkManagerFlags) {
 }
 
 func validateNetworkManagerFlags(managerFlags *networkManagerFlags) error {
-	cidrRegex := regexp.MustCompile(`^(\d{1,3}.){3}\d{1,3}(/(\d|[12]\d|3[012]))$`)
+	cidrRegex := regexp.MustCompile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]` +
+		`|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$`)
 
 	if !cidrRegex.MatchString(managerFlags.podCIDR) {
 		return fmt.Errorf("pod CIDR is empty or invalid (%q)", managerFlags.podCIDR)
@@ -172,10 +173,8 @@ func initializeIPAM(client dynamic.Interface, managerFlags *networkManagerFlags)
 		}
 	}
 
-	for _, pool := range managerFlags.reservedPools.StringList {
-		if err := ipam.AcquireReservedSubnet(pool); err != nil {
-			return nil, err
-		}
+	if err := ipam.SetReservedSubnets(managerFlags.reservedPools.StringList); err != nil {
+		return nil, err
 	}
 
 	return ipam, nil

deployments/liqo/crds/net.liqo.io_ipamstorages.yaml
@@ -121,6 +121,12 @@ spec:
                   Important: Run "make" to regenerate code after modifying this file
                   Map consumed by go-ipam module. Key is prefic cidr, value is a Prefix.'
                 type: object
+              reservedSubnets:
+                description: Reserved Networks. Subnets listed in this field are excluded
+                  from the list of possible subnets used for natting POD CIDR.
+                items:
+                  type: string
+                type: array
               serviceCIDR:
                 description: ServiceCIDR
                 type: string
@@ -132,6 +138,7 @@ spec:
             - podCIDR
             - pools
             - prefixes
+            - reservedSubnets
             - serviceCIDR
             type: object
         type: object

pkg/liqonet/ipam/ipam.go
@@ -457,22 +457,24 @@ func (liqoIPAM *IPAM) freePoolInHalves(pool string) error {
 		return err
 	}
 
-	klog.Infof("Network %s is equal to a network pool, freeing first half..", pool)
-	err = liqoIPAM.ipam.ReleaseChildPrefix(liqoIPAM.ipam.PrefixFrom(halfCidr))
-	if err != nil {
-		return fmt.Errorf("cannot free first half of pool %s: %w", pool, err)
+	klog.Infof("Network %s is equal to a network pool, freeing it in two steps...", pool)
+	for i := 1; i <= 2; i++ {
+		if i == 2 {
+			// Get second half CIDR
+			halfCidr, err = utils.Next(halfCidr)
+			if err != nil {
+				return err
+			}
+		}
+		klog.Infof("freeing half %d...", i)
+		if prefix := liqoIPAM.ipam.PrefixFrom(halfCidr); prefix != nil {
+			err = liqoIPAM.ipam.ReleaseChildPrefix(prefix)
+			if err != nil {
+				return fmt.Errorf("cannot free first half of pool %s: %w", pool, err)
+			}
+		}
 	}
 
-	// Get second half CIDR
-	halfCidr, err = utils.Next(halfCidr)
-	if err != nil {
-		return err
-	}
-	klog.Infof("Freeing second half..")
-	err = liqoIPAM.ipam.ReleaseChildPrefix(liqoIPAM.ipam.PrefixFrom(halfCidr))
-	if err != nil {
-		return fmt.Errorf("cannot free second half of pool %s: %w", pool, err)
-	}
 	klog.Infof("Network %s has successfully been freed", pool)
 	return nil
 }
@@ -1185,6 +1187,38 @@ func (liqoIPAM *IPAM) SetServiceCIDR(serviceCIDR string) error {
 	// Update Service CIDR
 	if err := liqoIPAM.ipamStorage.updateServiceCIDR(serviceCIDR); err != nil {
 		return fmt.Errorf("cannot set ServiceCIDR: %w", err)
+	}
+	return nil
+}
+
+// SetReservedSubnets acquires and/or frees the reserved networks.
+func (liqoIPAM *IPAM) SetReservedSubnets(subnets []string) error {
+	reserved := liqoIPAM.ipamStorage.getReservedSubnets()
+
+	// Free all the reserved networks not needed anymore.
+	for _, r := range reserved {
+		if !slice.ContainsString(subnets, r) {
+			klog.Infof("freeing old reserved subnet %s", r)
+			if err := liqoIPAM.FreeReservedSubnet(r); err != nil {
+				return fmt.Errorf("una error occurred while freeing reserved subnet {%s}: %w", r, err)
+			}
+			if err := liqoIPAM.ipamStorage.updateReservedSubnets(r, updateOpRemove); err != nil {
+				return err
+			}
+		}
+	}
+
+	// Reserve the newly added subnets.
+	for _, s := range subnets {
+		if !slice.ContainsString(reserved, s) {
+			klog.Infof("acquiring reserved subnet %s", s)
+			if err := liqoIPAM.AcquireReservedSubnet(s); err != nil {
+				return fmt.Errorf("an error occurred while reserving subnet {%s}: %w", s, err)
+			}
+			if err := liqoIPAM.ipamStorage.updateReservedSubnets(s, updateOpAdd); err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }

pkg/liqonet/ipam/ipamStorage.go
@@ -33,18 +33,22 @@ import (
 
 	netv1alpha1 "github.com/liqotech/liqo/apis/net/v1alpha1"
 	"github.com/liqotech/liqo/pkg/consts"
+	"github.com/liqotech/liqo/pkg/utils/slice"
 )
 
 const (
 	ipamNamePrefix              = "ipamstorage-"
 	clusterSubnetUpdate         = "clusterSubnets"
 	poolsUpdate                 = "pools"
+	reservedSubnetsUpdate       = "reservedSubnets"
 	prefixesUpdate              = "prefixes"
 	externalCIDRUpdate          = "externalCIDR"
 	endpointMappingsUpdate      = "endpointMappings"
 	podCIDRUpdate               = "podCIDR"
 	serviceCIDRUpdate           = "serviceCIDR"
 	natMappingsConfiguredUpdate = "natMappingsConfigured"
+	updateOpAdd                 = "add"
+	updateOpRemove              = "remove"
 )
 
 // IpamStorage is the interface to be implemented to enforce persistency in IPAM.
@@ -55,6 +59,7 @@ type IpamStorage interface {
 	updateEndpointMappings(endpoints map[string]netv1alpha1.EndpointMapping) error
 	updatePodCIDR(podCIDR string) error
 	updateServiceCIDR(serviceCIDR string) error
+	updateReservedSubnets(subnet, operation string) error
 	updateNatMappingsConfigured(natMappingsConfigured map[string]netv1alpha1.ConfiguredCluster) error
 	getClusterSubnets() map[string]netv1alpha1.Subnets
 	getPools() []string
@@ -62,6 +67,7 @@ type IpamStorage interface {
 	getEndpointMappings() map[string]netv1alpha1.EndpointMapping
 	getPodCIDR() string
 	getServiceCIDR() string
+	getReservedSubnets() []string
 	getNatMappingsConfigured() map[string]netv1alpha1.ConfiguredCluster
 	goipam.Storage
 }
@@ -206,21 +212,38 @@ func (ipamStorage *IPAMStorage) DeletePrefix(prefix goipam.Prefix) (goipam.Prefi
 func (ipamStorage *IPAMStorage) updateClusterSubnets(clusterSubnets map[string]netv1alpha1.Subnets) error {
 	return ipamStorage.updateConfig(clusterSubnetUpdate, clusterSubnets)
 }
+
 func (ipamStorage *IPAMStorage) updatePools(pools []string) error {
 	return ipamStorage.updateConfig(poolsUpdate, pools)
 }
+
+func (ipamStorage *IPAMStorage) updateReservedSubnets(subnet, operation string) error {
+	subnets := ipamStorage.getReservedSubnets()
+	switch operation {
+	case updateOpAdd:
+		subnets = append(subnets, subnet)
+	case updateOpRemove:
+		subnets = slice.RemoveString(subnets, subnet)
+	}
+	return ipamStorage.updateConfig(reservedSubnetsUpdate, subnets)
+}
+
 func (ipamStorage *IPAMStorage) updatePrefixes(prefixes map[string][]byte) error {
 	return ipamStorage.updateConfig(prefixesUpdate, prefixes)
 }
+
 func (ipamStorage *IPAMStorage) updateExternalCIDR(externalCIDR string) error {
 	return ipamStorage.updateConfig(externalCIDRUpdate, externalCIDR)
 }
+
 func (ipamStorage *IPAMStorage) updateEndpointMappings(endpoints map[string]netv1alpha1.EndpointMapping) error {
 	return ipamStorage.updateConfig(endpointMappingsUpdate, endpoints)
 }
+
 func (ipamStorage *IPAMStorage) updatePodCIDR(podCIDR string) error {
 	return ipamStorage.updateConfig(podCIDRUpdate, podCIDR)
 }
+
 func (ipamStorage *IPAMStorage) updateServiceCIDR(serviceCIDR string) error {
 	return ipamStorage.updateConfig(serviceCIDRUpdate, serviceCIDR)
 }
@@ -273,14 +296,21 @@ func (ipamStorage *IPAMStorage) getClusterSubnets() map[string]netv1alpha1.Subne
 func (ipamStorage *IPAMStorage) getExternalCIDR() string {
 	return ipamStorage.getConfig().Spec.ExternalCIDR
 }
+
 func (ipamStorage *IPAMStorage) getEndpointMappings() map[string]netv1alpha1.EndpointMapping {
 	return ipamStorage.getConfig().Spec.EndpointMappings
 }
+
 func (ipamStorage *IPAMStorage) getPodCIDR() string {
 	return ipamStorage.getConfig().Spec.PodCIDR
 }
+
 func (ipamStorage *IPAMStorage) getServiceCIDR() string {
 	return ipamStorage.getConfig().Spec.ServiceCIDR
+}
+
+func (ipamStorage *IPAMStorage) getReservedSubnets() []string {
+	return ipamStorage.getConfig().Spec.ReservedSubnets
 }
 
 func (ipamStorage *IPAMStorage) getNatMappingsConfigured() map[string]netv1alpha1.ConfiguredCluster {
@@ -342,6 +372,7 @@ func (ipamStorage *IPAMStorage) createConfig() (*netv1alpha1.IpamStorage, error)
 			ClusterSubnets:        make(map[string]netv1alpha1.Subnets),
 			EndpointMappings:      make(map[string]netv1alpha1.EndpointMapping),
 			NatMappingsConfigured: make(map[string]netv1alpha1.ConfiguredCluster),
+			ReservedSubnets:       []string{},
 		},
 	}
 

pkg/liqonet/ipam/ipam_test.go
@@ -1559,7 +1559,81 @@ var _ = Describe("Ipam", func() {
 			})
 		})
 	})
+
+	Describe("SetReservedSubnets", func() {
+		var (
+			toBeReservedSubnets1          []string
+			toBeReservedSubnets2          []string
+			toBeReservedSubnetsIncorrect1 []string
+		)
+
+		BeforeEach(func() {
+			toBeReservedSubnets1 = []string{"192.168.0.0/16", "100.200.0.0/16", "10.200.250.0/24"}
+			toBeReservedSubnets2 = []string{"192.168.1.0/24", "100.200.0.0/16", "172.16.34.0/24"}
+			toBeReservedSubnetsIncorrect1 = []string{"192.168.0.0/16", "100.200.0/16", "10.200.250.0/24"}
+		})
+		Context("Reserving subnets", func() {
+			When("Reserving subnets for the first time", func() {
+				It("should reserve all the the subnets and return nil", func() {
+					Expect(ipam.SetReservedSubnets(toBeReservedSubnets1)).To(BeNil())
+					Expect(ipam.ipamStorage.getReservedSubnets()).To(ContainElements(toBeReservedSubnets1))
+					checkForPrefixes(toBeReservedSubnets1)
+				})
+			})
+
+			When("Reserving subnets multiple times", func() {
+				It("should return nil", func() {
+					// Reserving the first time.
+					Expect(ipam.SetReservedSubnets(toBeReservedSubnets1)).To(BeNil())
+					Expect(ipam.ipamStorage.getReservedSubnets()).To(ContainElements(toBeReservedSubnets1))
+					// Reserving the second time.
+					Expect(ipam.SetReservedSubnets(toBeReservedSubnets1)).To(BeNil())
+					Expect(ipam.ipamStorage.getReservedSubnets()).To(ContainElements(toBeReservedSubnets1))
+					checkForPrefixes(toBeReservedSubnets1)
+				})
+			})
+
+			When("Reserving a list of subnets with incorrect ones", func() {
+				It("should reserve all the correct ones that comes before the incorrect one", func() {
+					Expect(ipam.SetReservedSubnets(toBeReservedSubnetsIncorrect1)).To(HaveOccurred())
+					Expect(ipam.ipamStorage.getReservedSubnets()).To(HaveLen(1))
+					Expect(ipam.ipamStorage.getReservedSubnets()).To(ContainElement(toBeReservedSubnetsIncorrect1[0]))
+				})
+			})
+		})
+
+		Context("Making available subnets previously reserved", func() {
+			JustBeforeEach(func() {
+				// Reserve the subnets.
+				Expect(ipam.SetReservedSubnets(toBeReservedSubnets1)).To(BeNil())
+				Expect(ipam.ipamStorage.getReservedSubnets()).To(ContainElements(toBeReservedSubnets1))
+				checkForPrefixes(toBeReservedSubnets1)
+			})
+			When("reserved subnets are no more needed", func() {
+				It("should remove all the previously reserved networks", func() {
+					Expect(ipam.SetReservedSubnets(nil)).To(BeNil())
+					Expect(ipam.ipamStorage.getReservedSubnets()).Should(HaveLen(0))
+				})
+			})
+
+			When("new subnets are reserved and existing ones are freed", func() {
+				It("should return nil and update the reserved subnets", func() {
+					Expect(ipam.SetReservedSubnets(toBeReservedSubnets2)).To(BeNil())
+					Expect(ipam.ipamStorage.getReservedSubnets()).To(ContainElements(toBeReservedSubnets2))
+					checkForPrefixes(toBeReservedSubnets2)
+				})
+			})
+		})
+	})
 })
+
+func checkForPrefixes(subnets []string) {
+	for _, s := range subnets {
+		prefix, err := ipam.ipamStorage.ReadPrefix(s)
+		Expect(err).To(BeNil())
+		Expect(prefix.Cidr).To(Equal(s))
+	}
+}
 
 func getNatMappingResourcePerCluster(clusterID string) (*liqonetapi.NatMapping, error) {
 	nm := &liqonetapi.NatMapping{}