9a2961cda0ea842d428977e7b94ede9e69e4b0e8
CapacitorSet <capacitorset@gmail.com>
2021-11-20 14:39:35 +0100

    liqoctl: generate random cluster name if none is provided

--- pkg/liqoctl/install/provider/provider_suite_test.go
+++ pkg/liqoctl/install/provider/provider_suite_test.go
@@ -18,10 +18,54 @@ import (
 	"testing"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/spf13/pflag"
+
+	"github.com/liqotech/liqo/pkg/liqoctl/install/provider"
 )
 
 func TestProvider(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Provider Suite")
 }
+
+func newFlagSet() *pflag.FlagSet {
+	flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
+	flags.String("cluster-name", "", "")
+	flags.String("reserved-subnets", "", "")
+	return flags
+}
+
+type commandTestcase struct {
+	flags                     map[string]string
+	expectedValidationOutcome OmegaMatcher
+	expectedClusterName       OmegaMatcher
+}
+
+var _ = DescribeTable("ValidateGenericCommandArguments",
+	func(t commandTestcase) {
+		flags := newFlagSet()
+		for flag, value := range t.flags {
+			Expect(flags.Set(flag, value)).To(Succeed())
+		}
+		provider := provider.GenericProvider{}
+		Expect(provider.ValidateGenericCommandArguments(flags)).To(t.expectedValidationOutcome)
+		Expect(provider.ClusterName).To(t.expectedClusterName)
+	},
+	Entry("should accept a valid name", commandTestcase{
+		flags:                     map[string]string{"cluster-name": "test-cluster123"},
+		expectedValidationOutcome: Succeed(),
+		expectedClusterName:       Equal("test-cluster123"),
+	}),
+	Entry("should not accept an invalid name", commandTestcase{
+		flags:                     map[string]string{"cluster-name": "Invalid cluster!"},
+		expectedValidationOutcome: Not(Succeed()),
+		expectedClusterName:       BeEmpty(),
+	}),
+	Entry("should generate a valid name if none is given", commandTestcase{
+		flags:                     map[string]string{},
+		expectedValidationOutcome: Succeed(),
+		expectedClusterName:       Not(BeEmpty()),
+	}),
+)
