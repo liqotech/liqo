26e191665110698a557a521d9293cb98c4bea368
CapacitorSet <capacitorset@gmail.com>
2021-12-09 20:06:22 +0100

    Lint

--- cmd/liqo-controller-manager/main.go
+++ cmd/liqo-controller-manager/main.go
@@ -255,7 +255,8 @@ func main() {
 	var resourceRequestReconciler *resourceRequestOperator.ResourceRequestReconciler
 	monitor := resourceRequestOperator.NewLocalMonitor(ctx, clientset, *resyncPeriod)
 	scaledMonitor := &resourceRequestOperator.ResourceScaler{Provider: monitor, Factor: float32(resourceSharingPercentage.Val) / 100.}
-	offerUpdater := resourceRequestOperator.NewOfferUpdater(mgr.GetClient(), clusterIdentity, clusterLabels.StringMap, scaledMonitor, uint(offerUpdateThreshold.Val), *realStorageClassName, *enableStorage)
+	offerUpdater := resourceRequestOperator.NewOfferUpdater(mgr.GetClient(), clusterIdentity, clusterLabels.StringMap,
+		scaledMonitor, uint(offerUpdateThreshold.Val), *realStorageClassName, *enableStorage)
 	resourceRequestReconciler = &resourceRequestOperator.ResourceRequestReconciler{
 		Client:                mgr.GetClient(),
 		Scheme:                mgr.GetScheme(),
