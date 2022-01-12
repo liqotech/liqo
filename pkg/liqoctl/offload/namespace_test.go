6c45ffbf89fdc8be4258a5b660d8b25d3d581a8c
Alessandro Olivero <aleoli96@gmail.com>
2022-01-12 13:38:31 +0000

    fix liqoctl offload unit test

--- pkg/liqoctl/offload/namespace_test.go
+++ pkg/liqoctl/offload/namespace_test.go
@@ -20,6 +20,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -41,7 +42,7 @@ var _ = Describe("Test the generate command works as expected", func() {
 		parameters     []string
 		acceptedLabels args.StringMap
 		deniedLabels   args.StringMap
-		expected       *v1alpha1.NamespaceOffloading
+		expected       v1alpha1.NamespaceOffloading
 	}
 
 	DescribeTable("A generate command is performed",
@@ -51,14 +52,23 @@ var _ = Describe("Test the generate command works as expected", func() {
 			cmd.PersistentFlags().String(PodOffloadingStrategyFlag, string(v1alpha1.LocalAndRemotePodOffloadingStrategyType), "")
 			cmd.PersistentFlags().String(NamespaceMappingStrategyFlag, string(v1alpha1.DefaultNameMappingStrategyType), "")
 			Expect(cmd.Execute()).To(Succeed())
-			Expect(forgeNamespaceOffloading(cmd, tc.args, tc.acceptedLabels, tc.deniedLabels)).To(Equal(tc.expected))
+			nsOffloading := forgeNamespaceOffloading(cmd, tc.args, tc.acceptedLabels, tc.deniedLabels)
+			Expect(nsOffloading.ObjectMeta).To(MatchFields(IgnoreExtras, Fields{
+				"Name":      Equal(tc.expected.Name),
+				"Namespace": Equal(tc.expected.Namespace),
+			}))
+			Expect(nsOffloading.Spec).To(MatchFields(IgnoreExtras, Fields{
+				"NamespaceMappingStrategy": Equal(tc.expected.Spec.NamespaceMappingStrategy),
+				"PodOffloadingStrategy":    Equal(tc.expected.Spec.PodOffloadingStrategy),
+				"ClusterSelector":          Equal(tc.expected.Spec.ClusterSelector),
+			}))
 		},
 		Entry("Offload namespace with default parameters", testCase{
 			[]string{"test"},
 			[]string{},
 			args.StringMap{StringMap: map[string]string{}},
 			args.StringMap{StringMap: map[string]string{}},
-			&v1alpha1.NamespaceOffloading{
+			v1alpha1.NamespaceOffloading{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      liqoconst.DefaultNamespaceOffloadingName,
 					Namespace: "test",
@@ -88,7 +98,7 @@ var _ = Describe("Test the generate command works as expected", func() {
 				args.StringMap{StringMap: map[string]string{
 					"denied": "true",
 				}},
-				&v1alpha1.NamespaceOffloading{
+				v1alpha1.NamespaceOffloading{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      liqoconst.DefaultNamespaceOffloadingName,
 						Namespace: "test",
@@ -132,7 +142,7 @@ var _ = Describe("Test the generate command works as expected", func() {
 			args.StringMap{StringMap: map[string]string{
 				"denied": "true",
 			}},
-			&v1alpha1.NamespaceOffloading{
+			v1alpha1.NamespaceOffloading{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      liqoconst.DefaultNamespaceOffloadingName,
 					Namespace: "test",
@@ -176,7 +186,7 @@ var _ = Describe("Test the generate command works as expected", func() {
 					"test": "true",
 				}},
 				args.StringMap{StringMap: map[string]string{}},
-				&v1alpha1.NamespaceOffloading{
+				v1alpha1.NamespaceOffloading{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      liqoconst.DefaultNamespaceOffloadingName,
 						Namespace: "test",
@@ -189,14 +199,14 @@ var _ = Describe("Test the generate command works as expected", func() {
 								{
 									MatchExpressions: []corev1.NodeSelectorRequirement{
 										{
-											Key:      "test",
-											Operator: corev1.NodeSelectorOpIn,
-											Values:   []string{"true"},
-										},
-										{
 											Key:      liqoconst.TypeLabel,
 											Operator: corev1.NodeSelectorOpIn,
 											Values:   []string{liqoconst.TypeNode},
+										},
+										{
+											Key:      "test",
+											Operator: corev1.NodeSelectorOpIn,
+											Values:   []string{"true"},
 										},
 									},
 								},
@@ -215,7 +225,7 @@ var _ = Describe("Test the generate command works as expected", func() {
 				args.StringMap{StringMap: map[string]string{
 					"test": "true",
 				}},
-				&v1alpha1.NamespaceOffloading{
+				v1alpha1.NamespaceOffloading{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      liqoconst.DefaultNamespaceOffloadingName,
 						Namespace: "test",
