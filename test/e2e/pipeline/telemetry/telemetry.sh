b10ff44c77311c0bb47f9aedf979aee8ef51a66d
Francesco Cheinasso <francesco.cheinasso@staff.aruba.it>
2023-08-22 16:49:43 +0200

    E2E: telemetry fix

--- test/e2e/pipeline/telemetry/telemetry.sh
+++ test/e2e/pipeline/telemetry/telemetry.sh
@@ -5,6 +5,7 @@
 # NAMESPACE             -> the namespace where liqo is running
 # LIQO_VERSION          -> the liqo version to test
 # K8S_VERSION           -> the Kubernetes version
+# TMPDIR                -> the directory where kubeconfigs are stored
 
 set -e           # Fail in case of error
 set -o nounset   # Fail if undefined variables are used
@@ -19,6 +20,6 @@ trap 'error "${BASH_SOURCE}" "${LINENO}"' ERR
 
 for i in $(seq 1 "${CLUSTER_NUMBER}")
 do
-  export KUBECONFIG="${TMPDIR2}/kubeconfigs/liqo_kubeconf_${i}"
+  export KUBECONFIG="${TMPDIR}/kubeconfigs/liqo_kubeconf_${i}"
   go run ./cmd/telemetry/main.go --liqo-version "${LIQO_VERSION}" --kubernetes-version "${K8S_VERSION}" --dry-run
 done
