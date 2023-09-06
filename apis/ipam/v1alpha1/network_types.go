259bff73ac9b721e4af4e333a106451350db4ed9
Francesco Torta <62566275+fra98@users.noreply.github.com>
2023-09-06 09:28:44 +0000

    IP: create associated Service

--- apis/ipam/v1alpha1/network_types.go
+++ apis/ipam/v1alpha1/network_types.go
@@ -46,6 +46,7 @@ type NetworkStatus struct {
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:categories=liqo
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Desired CIDR",type=string,JSONPath=`.spec.cidr`
 // +kubebuilder:printcolumn:name="Remapped CIDR",type=string,JSONPath=`.status.cidr`
