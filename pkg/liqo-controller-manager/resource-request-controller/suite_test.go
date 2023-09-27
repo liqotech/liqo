320f059fae404852b61baaf21ce15f2b9f408ecc
Francesco Cheinasso <francesco.cheinasso@staff.aruba.it>
2023-09-27 18:17:08 +0200

    Unit tests: envtest server new crds path

--- pkg/liqo-controller-manager/resource-request-controller/suite_test.go
+++ pkg/liqo-controller-manager/resource-request-controller/suite_test.go
@@ -63,7 +63,7 @@ func createCluster() {
 	ctx, cancel = context.WithCancel(context.Background())
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths: []string{
-			filepath.Join("..", "..", "..", "deployments", "liqo", "crds"),
+			filepath.Join("..", "..", "..", "deployments", "liqo", "charts", "liqo-crds", "crds"),
 		},
 	}
 
