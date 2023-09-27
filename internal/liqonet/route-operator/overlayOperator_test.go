1ebe65cef8e1e783e1e5389e38eb66aaf4201799
Francesco Cheinasso <francesco.cheinasso@staff.aruba.it>
2023-09-27 18:17:08 +0200

    Unit tests: envtest server new crds path

--- internal/liqonet/route-operator/overlayOperator_test.go
+++ internal/liqonet/route-operator/overlayOperator_test.go
@@ -394,7 +394,9 @@ func setupOverlayTestEnv() error {
 		return err
 	}
 	overlayEnvTest = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "..", "..", "deployments", "liqo", "crds")},
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "..", "..", "deployments", "liqo", "charts", "liqo-crds", "crds"),
+		},
 	}
 	config, err := overlayEnvTest.Start()
 	if err != nil {
