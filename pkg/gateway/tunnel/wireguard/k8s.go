101a7008729f022e26b98d5ef7828c4af56b98b3
Francesco Cheinasso <francesco.cheinasso@staff.aruba.it>
2023-09-28 12:32:11 +0200

    Wireguard Tunnel Container

---
+++ pkg/gateway/tunnel/wireguard/k8s.go
@@ -0,0 +1,122 @@
+// Copyright 2019-2023 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wireguard
+
+import (
+	"context"
+	"fmt"
+
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	networkingv1alpha1 "github.com/liqotech/liqo/apis/networking/v1alpha1"
+	"github.com/liqotech/liqo/pkg/consts"
+	"github.com/liqotech/liqo/pkg/gateway/tunnel/common"
+)
+
+// SetOwnerReferenceWithMode sets the owner reference of the object according to the mode.
+func SetOwnerReferenceWithMode(opts *Options, obj metav1.Object, scheme *runtime.Scheme) error {
+	meta := metav1.ObjectMeta{
+		Name:      opts.Name,
+		Namespace: opts.Namespace,
+		UID:       types.UID(opts.GatewayUID),
+	}
+	switch opts.Mode {
+	case common.ModeServer:
+		return controllerutil.SetOwnerReference(&networkingv1alpha1.GatewayServer{ObjectMeta: meta}, obj, scheme)
+	case common.ModeClient:
+		return controllerutil.SetOwnerReference(&networkingv1alpha1.GatewayClient{ObjectMeta: meta}, obj, scheme)
+	}
+	return fmt.Errorf("invalid mode %v", opts.Mode)
+}
+
+// CheckKeysSecret checks if the keys secret exists and if it contains the private and public keys.
+func CheckKeysSecret(ctx context.Context, cl client.Client, opts *Options) (wgtypes.Key, error) {
+	secret := &corev1.Secret{}
+	if err := cl.Get(ctx, types.NamespacedName{Name: GenerateResourceName(opts.Name), Namespace: opts.Namespace}, secret); err != nil {
+		return wgtypes.Key{}, err
+	}
+	if secret.Data == nil {
+		return wgtypes.Key{}, nil
+	}
+	if k, ok := secret.Data[consts.PrivateKeyField]; !ok || len(k) != wgtypes.KeyLen {
+		return wgtypes.Key{}, nil
+	}
+	if k, ok := secret.Data[consts.PublicKeyField]; !ok || len(k) != wgtypes.KeyLen {
+		return wgtypes.Key{}, nil
+	}
+	return wgtypes.Key(secret.Data[consts.PrivateKeyField]), nil
+}
+
+// CreateKeysSecret creates the private and public keys for the Wireguard interface and save them inside a Secret resource.
+func CreateKeysSecret(ctx context.Context, cl client.Client, opts *Options, pri, pub wgtypes.Key) error {
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      GenerateResourceName(opts.Name),
+			Namespace: opts.Namespace,
+		},
+	}
+
+	if _, err := controllerutil.CreateOrUpdate(ctx, cl, secret, func() error {
+		secret.SetLabels(map[string]string{
+			string(consts.RemoteClusterID):      opts.RemoteClusterID,
+			string(consts.GatewayResourceLabel): string(consts.GatewayResourceLabelValue),
+		})
+		if err := SetOwnerReferenceWithMode(opts, secret, cl.Scheme()); err != nil {
+			return err
+		}
+		secret.Data = map[string][]byte{
+			consts.PrivateKeyField: pri[:],
+			consts.PublicKeyField:  pub[:],
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// EnsureConnection creates or updates the connection resource.
+func EnsureConnection(ctx context.Context, cl client.Client, scheme *runtime.Scheme, opts *Options) error {
+	conn := &networkingv1alpha1.Connection{ObjectMeta: metav1.ObjectMeta{
+		Name: GenerateResourceName(opts.Name), Namespace: opts.Namespace,
+	}}
+	_, err := controllerutil.CreateOrUpdate(ctx, cl, conn, func() error {
+		if err := SetOwnerReferenceWithMode(opts, conn, scheme); err != nil {
+			return err
+		}
+		conn.Spec.GatewayRef.APIVersion = networkingv1alpha1.GroupVersion.String()
+		conn.Spec.GatewayRef.Name = opts.Name
+		conn.Spec.GatewayRef.Namespace = opts.Namespace
+		conn.Spec.GatewayRef.UID = types.UID(opts.GatewayUID)
+		switch opts.Mode {
+		case common.ModeServer:
+			conn.Spec.Type = networkingv1alpha1.ConnectionTypeServer
+			conn.Spec.GatewayRef.Kind = networkingv1alpha1.WgGatewayServerKind
+		case common.ModeClient:
+			conn.Spec.Type = networkingv1alpha1.ConnectionTypeClient
+			conn.Spec.GatewayRef.Kind = networkingv1alpha1.WgGatewayClientKind
+		}
+		return nil
+	})
+
+	return err
+}
