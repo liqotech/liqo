eed373f02eb84e4e2f16f152b114add3b0745a4c
Alessandro Olivero <aleoli96@gmail.com>
2023-10-03 11:33:52 +0200

    liqoctl add network configuration commands

--- cmd/liqoctl/main.go
+++ cmd/liqoctl/main.go
@@ -26,6 +26,7 @@ import (
 
 	discoveryv1alpha1 "github.com/liqotech/liqo/apis/discovery/v1alpha1"
 	netv1alpha1 "github.com/liqotech/liqo/apis/net/v1alpha1"
+	networkingv1alpha1 "github.com/liqotech/liqo/apis/networking/v1alpha1"
 	offloadingv1alpha1 "github.com/liqotech/liqo/apis/offloading/v1alpha1"
 	sharingv1alpha1 "github.com/liqotech/liqo/apis/sharing/v1alpha1"
 	virtualkubeletv1alpha1 "github.com/liqotech/liqo/apis/virtualkubelet/v1alpha1"
@@ -38,6 +39,7 @@ func init() {
 	utilruntime.Must(offloadingv1alpha1.AddToScheme(scheme.Scheme))
 	utilruntime.Must(sharingv1alpha1.AddToScheme(scheme.Scheme))
 	utilruntime.Must(virtualkubeletv1alpha1.AddToScheme(scheme.Scheme))
+	utilruntime.Must(networkingv1alpha1.AddToScheme(scheme.Scheme))
 }
 
 func main() {
