3221211784fd52a4ec1c5e420d8a55032518e60d
Francesco Cheinasso <francesco.cheinasso@staff.aruba.it>
2023-10-10 14:02:16 +0200

    External Network: connection controller

--- pkg/gateway/tunnel/wireguard/flags.go
+++ pkg/gateway/tunnel/wireguard/flags.go
@@ -20,7 +20,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 
-	"github.com/liqotech/liqo/pkg/gateway/tunnel/common"
+	"github.com/liqotech/liqo/pkg/gateway"
 )
 
 // FlagName is the type for the name of the flags.
@@ -31,17 +31,9 @@ func (fn FlagName) String() string {
 }
 
 const (
-	// FlagNameName is the name of the WgGateway resource.
-	FlagNameName FlagName = "name"
-	// FlagNameNamespace is the namespace WgGateway resource.
-	FlagNameNamespace FlagName = "namespace"
-	// FlagNameRemoteClusterID is the clusterID of the remote cluster.
-	FlagNameRemoteClusterID FlagName = "remote-cluster-id"
 	// FlagNameGatewayUID is the UID of the wireguard gateway.
 	FlagNameGatewayUID FlagName = "gateway-uid"
 
-	// FlagNameMode is the mode in which the wireguard interface is configured.
-	FlagNameMode FlagName = "mode"
 	// FlagNameMTU is the MTU for the wireguard interface.
 	FlagNameMTU FlagName = "mtu"
 	// FlagNameListenPort is the listen port for the wireguard interface.
@@ -57,29 +49,11 @@ const (
 
 	// FlagNameDNSCheckInterval is the interval between two DNS checks.
 	FlagNameDNSCheckInterval FlagName = "dns-check-interval"
-
-	// FlagNameLeaderElection is the flag to enable leader election.
-	FlagNameLeaderElection FlagName = "leader-election"
-	// FlagNameLeaderElectionLeaseDuration is the lease duration for the leader election.
-	FlagNameLeaderElectionLeaseDuration FlagName = "leader-election-lease-duration"
-	// FlagNameLeaderElectionRenewDeadline is the renew deadline for the leader election.
-	FlagNameLeaderElectionRenewDeadline FlagName = "leader-election-renew-deadline"
-	// FlagNameLeaderElectionRetryPeriod is the retry period for the leader election.
-	FlagNameLeaderElectionRetryPeriod FlagName = "leader-election-retry-period"
-
-	// FlagNameMetricsAddress is the address for the metrics endpoint.
-	FlagNameMetricsAddress FlagName = "metrics-address"
-	// FlagNameProbeAddr is the address for the health probe endpoint.
-	FlagNameProbeAddr FlagName = "health-probe-bind-address"
 )
 
 // RequiredFlags contains the list of the mandatory flags.
 var RequiredFlags = []FlagName{
-	FlagNameName,
-	FlagNameNamespace,
-	FlagNameRemoteClusterID,
 	FlagNameGatewayUID,
-	FlagNameMode,
 }
 
 // ClientRequiredFlags contains the list of the mandatory flags for the client mode.
@@ -89,12 +63,8 @@ var ClientRequiredFlags = []FlagName{
 
 // InitFlags initializes the flags for the wireguard tunnel.
 func InitFlags(flagset *pflag.FlagSet, opts *Options) {
-	flagset.StringVar(&opts.Name, FlagNameName.String(), "", "Parent gateway name")
-	flagset.StringVar(&opts.Namespace, FlagNameNamespace.String(), "", "Parent gateway namespace")
-	flagset.StringVar(&opts.RemoteClusterID, FlagNameRemoteClusterID.String(), "", "ClusterID of the remote cluster")
 	flagset.StringVar(&opts.GatewayUID, FlagNameGatewayUID.String(), "", "Parent gateway resource UID")
 
-	flagset.Var(&opts.Mode, FlagNameMode.String(), "Parent gateway mode")
 	flagset.IntVar(&opts.MTU, FlagNameMTU.String(), 1420, "MTU for the interface")
 	flagset.StringVar(&opts.InterfaceName, FlagNameInterfaceName.String(), "liqo-tunnel", "Name for the tunnel interface")
 	flagset.IntVar(&opts.ListenPort, FlagNameListenPort.String(), 51820, "Listen port (server only)")
@@ -102,17 +72,6 @@ func InitFlags(flagset *pflag.FlagSet, opts *Options) {
 	flagset.IntVar(&opts.EndpointPort, FlagNameEndpointPort.String(), 51820, "Endpoint port (client only)")
 
 	flagset.DurationVar(&opts.DNSCheckInterval, FlagNameDNSCheckInterval.String(), 5*time.Minute, "Interval between two DNS checks")
-
-	flagset.BoolVar(&opts.LeaderElection, FlagNameLeaderElection.String(), false, "Enable leader election")
-	flagset.DurationVar(&opts.LeaderElectionLeaseDuration, FlagNameLeaderElectionLeaseDuration.String(), 15*time.Second,
-		"LeaseDuration for the leader election")
-	flagset.DurationVar(&opts.LeaderElectionRenewDeadline, FlagNameLeaderElectionRenewDeadline.String(), 10*time.Second,
-		"RenewDeadline for the leader election")
-	flagset.DurationVar(&opts.LeaderElectionRetryPeriod, FlagNameLeaderElectionRetryPeriod.String(), 2*time.Second,
-		"RetryPeriod for the leader election")
-
-	flagset.StringVar(&opts.MetricsAddress, FlagNameMetricsAddress.String(), ":8080", "Address for the metrics endpoint")
-	flagset.StringVar(&opts.ProbeAddr, FlagNameProbeAddr.String(), ":8081", "Address for the health probe endpoint")
 }
 
 // MarkFlagsRequired marks the flags as required.
@@ -122,7 +81,7 @@ func MarkFlagsRequired(cmd *cobra.Command, opts *Options) error {
 			return err
 		}
 	}
-	if opts.Mode == common.ModeClient {
+	if opts.GwOptions.Mode == gateway.ModeClient {
 		for _, flag := range ClientRequiredFlags {
 			if err := cmd.MarkFlagRequired(flag.String()); err != nil {
 				return err
