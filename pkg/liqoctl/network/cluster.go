4c05705f85d39a5375e17ab5a8658b52b07f7829
Francesco Torta <62566275+fra98@users.noreply.github.com>
2023-10-10 16:21:22 +0200

    Liqoctl network connect

--- pkg/liqoctl/network/cluster.go
+++ pkg/liqoctl/network/cluster.go
@@ -27,6 +27,9 @@ import (
 	"github.com/liqotech/liqo/pkg/liqoctl/factory"
 	"github.com/liqotech/liqo/pkg/liqoctl/output"
 	"github.com/liqotech/liqo/pkg/liqoctl/rest/configuration"
+	"github.com/liqotech/liqo/pkg/liqoctl/rest/gatewayclient"
+	"github.com/liqotech/liqo/pkg/liqoctl/rest/gatewayserver"
+	"github.com/liqotech/liqo/pkg/liqoctl/rest/publickey"
 	"github.com/liqotech/liqo/pkg/liqoctl/wait"
 	liqogetters "github.com/liqotech/liqo/pkg/utils/getters"
 	liqolabels "github.com/liqotech/liqo/pkg/utils/labels"
@@ -54,6 +57,26 @@ func NewCluster(local, remote *factory.Factory) *Cluster {
 
 // Init initializes the cluster struct.
 func (c *Cluster) Init(ctx context.Context) error {
+	// Set cluster identity.
+	if err := c.SetClusterIdentity(ctx); err != nil {
+		return err
+	}
+
+	// Get network configuration.
+	s := c.local.Printer.StartSpinner("Retrieving network configuration")
+	conf, err := configuration.ForgeLocalConfiguration(ctx, c.local.CRClient, c.local.Namespace, c.local.LiqoNamespace)
+	if err != nil {
+		s.Fail(fmt.Sprintf("An error occurred while retrieving network configuration: %v", output.PrettyErr(err)))
+		return err
+	}
+	c.NetworkConfiguration = conf
+	s.Success("Network configuration correctly retrieved")
+
+	return nil
+}
+
+// SetClusterIdentity set cluster identity retrieving it from the Liqo configmap.
+func (c *Cluster) SetClusterIdentity(ctx context.Context) error {
 	// Get cluster identity.
 	s := c.local.Printer.StartSpinner("Retrieving cluster identity")
 	selector, err := metav1.LabelSelectorAsSelector(&liqolabels.ClusterIDConfigMapLabelSelector)
@@ -73,17 +96,6 @@ func (c *Cluster) Init(ctx context.Context) error {
 	}
 	c.clusterIdentity = clusterIdentity
 	s.Success("Cluster identity correctly retrieved")
-
-	// Get network configuration.
-	s = c.local.Printer.StartSpinner("Retrieving network configuration")
-	conf, err := configuration.ForgeLocalConfiguration(ctx, c.local.CRClient, c.local.Namespace, c.local.LiqoNamespace)
-	if err != nil {
-		s.Fail(fmt.Sprintf("An error occurred while retrieving network configuration: %v", output.PrettyErr(err)))
-		return err
-	}
-	c.NetworkConfiguration = conf
-	s.Success("Network configuration correctly retrieved")
-
 	return nil
 }
 
@@ -111,5 +123,65 @@ func (c *Cluster) SetupConfiguration(ctx context.Context,
 	}
 
 	s.Success("Network configuration correctly set up")
+	return nil
+}
+
+// EnsureGatewayServer create or updates a GatewayServer.
+func (c *Cluster) EnsureGatewayServer(ctx context.Context, name string, opts *gatewayserver.ForgeOptions) (*networkingv1alpha1.GatewayServer, error) {
+	s := c.local.Printer.StartSpinner("Setting up Gateway Server")
+	gwServer, err := gatewayserver.ForgeGatewayServer(name, c.local.Namespace, opts)
+	if err != nil {
+		s.Fail(fmt.Sprintf("An error occurred while forging gatewayserver: %v", output.PrettyErr(err)))
+		return nil, err
+	}
+	_, err = controllerutil.CreateOrUpdate(ctx, c.local.CRClient, gwServer, func() error {
+		return gatewayserver.MutateGatewayServer(gwServer, opts)
+	})
+	if err != nil {
+		s.Fail(fmt.Sprintf("An error occurred while setting up gatewayserver: %v", output.PrettyErr(err)))
+		return nil, err
+	}
+
+	s.Success("Gatewayserver correctly set up")
+	return gwServer, nil
+}
+
+// EnsureGatewayClient create or updates a GatewayClient.
+func (c *Cluster) EnsureGatewayClient(ctx context.Context, name string, opts *gatewayclient.ForgeOptions) (*networkingv1alpha1.GatewayClient, error) {
+	s := c.local.Printer.StartSpinner("Setting up Gateway Client")
+	gwClient, err := gatewayclient.ForgeGatewayClient(name, c.local.Namespace, opts)
+	if err != nil {
+		s.Fail(fmt.Sprintf("An error occurred while forging gatewayclient: %v", output.PrettyErr(err)))
+		return nil, err
+	}
+	_, err = controllerutil.CreateOrUpdate(ctx, c.local.CRClient, gwClient, func() error {
+		return gatewayclient.MutateGatewayClient(gwClient, opts)
+	})
+	if err != nil {
+		s.Fail(fmt.Sprintf("An error occurred while setting up gatewayclient: %v", output.PrettyErr(err)))
+		return nil, err
+	}
+
+	s.Success("Gatewayclient correctly set up")
+	return gwClient, nil
+}
+
+// EnsurePublicKey create or updates a PublicKey.
+func (c *Cluster) EnsurePublicKey(ctx context.Context, remoteClusterIdentity *discoveryv1alpha1.ClusterIdentity, key []byte) error {
+	s := c.local.Printer.StartSpinner("Creating PublicKey")
+	pubKey, err := publickey.ForgePublicKey(remoteClusterIdentity.ClusterName, c.local.Namespace, remoteClusterIdentity.ClusterID, key)
+	if err != nil {
+		s.Fail(fmt.Sprintf("An error occurred while forging publickey: %v", output.PrettyErr(err)))
+		return err
+	}
+	_, err = controllerutil.CreateOrUpdate(ctx, c.local.CRClient, pubKey, func() error {
+		return publickey.MutatePublicKey(pubKey, remoteClusterIdentity.ClusterID, key)
+	})
+	if err != nil {
+		s.Fail(fmt.Sprintf("An error occurred while creating publickey: %v", output.PrettyErr(err)))
+		return err
+	}
+
+	s.Success("PublicKey correctly created")
 	return nil
 }
