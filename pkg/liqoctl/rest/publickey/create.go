094c0ec40bf023bcfefa9f6bcf5ae72d16b4c795
Francesco Torta <62566275+fra98@users.noreply.github.com>
2023-10-10 16:21:22 +0200

    Liqoctl network connect

---
+++ pkg/liqoctl/rest/publickey/create.go
@@ -0,0 +1,131 @@
+// Copyright 2019-2023 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package publickey
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/cli-runtime/pkg/printers"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	networkingv1alpha1 "github.com/liqotech/liqo/apis/networking/v1alpha1"
+	"github.com/liqotech/liqo/pkg/liqoctl/completion"
+	"github.com/liqotech/liqo/pkg/liqoctl/output"
+	"github.com/liqotech/liqo/pkg/liqoctl/rest"
+	"github.com/liqotech/liqo/pkg/utils/args"
+)
+
+const liqoctlCreatePublicKeyLongHelp = `Create a PublicKey.
+
+The PublicKey resource is used to define a PublicKey for the external network.
+
+Examples:
+  $ {{ .Executable }} create publickey my-public-key --cluster-id my-cluster-id --type server --gateway-name my-gateway`
+
+// Create creates a PublicKey.
+func (o *Options) Create(ctx context.Context, options *rest.CreateOptions) *cobra.Command {
+	outputFormat := args.NewEnum([]string{"json", "yaml"}, "")
+
+	o.createOptions = options
+
+	cmd := &cobra.Command{
+		Use:     "publickey",
+		Aliases: []string{"publickeys", "publickeies"},
+		Short:   "Create a Public Key",
+		Long:    liqoctlCreatePublicKeyLongHelp,
+		Args:    cobra.ExactArgs(1),
+
+		PreRun: func(cmd *cobra.Command, args []string) {
+			options.OutputFormat = outputFormat.Value
+			options.Name = args[0]
+			o.createOptions = options
+		},
+
+		Run: func(cmd *cobra.Command, args []string) {
+			output.ExitOnErr(o.handleCreate(ctx))
+		},
+	}
+
+	cmd.Flags().VarP(outputFormat, "output", "o",
+		"Output the resulting PublicKey resource, instead of applying it. Supported formats: json, yaml")
+
+	cmd.Flags().StringVar(&o.ClusterID, "cluster-id", "", "The cluster ID of the remote cluster")
+	cmd.Flags().BytesBase64Var(&o.PublicKey, "public-key", nil, "The public key to be used for the Gateway")
+
+	runtime.Must(cmd.MarkFlagRequired("cluster-id"))
+	runtime.Must(cmd.MarkFlagRequired("public-key"))
+
+	runtime.Must(cmd.RegisterFlagCompletionFunc("output", completion.Enumeration(outputFormat.Allowed)))
+	runtime.Must(cmd.RegisterFlagCompletionFunc("cluster-id", completion.ClusterIDs(ctx,
+		o.createOptions.Factory, completion.NoLimit)))
+
+	return cmd
+}
+
+func (o *Options) handleCreate(ctx context.Context) error {
+	opts := o.createOptions
+
+	pubKey, err := ForgePublicKey(opts.Name, opts.Namespace, o.ClusterID, o.PublicKey)
+	if err != nil {
+		opts.Printer.CheckErr(err)
+		return err
+	}
+
+	if opts.OutputFormat != "" {
+		opts.Printer.CheckErr(o.output(pubKey))
+		return nil
+	}
+
+	s := opts.Printer.StartSpinner("Creating publickey")
+
+	_, err = controllerutil.CreateOrUpdate(ctx, opts.CRClient, pubKey, func() error {
+		return MutatePublicKey(pubKey, o.ClusterID, o.PublicKey)
+	})
+	if err != nil {
+		s.Fail("Unable to create publickey: %v", output.PrettyErr(err))
+		return err
+	}
+	s.Success("Publickey created")
+
+	return nil
+}
+
+// output implements the logic to output the generated PublicKey resource.
+func (o *Options) output(pubKey *networkingv1alpha1.PublicKey) error {
+	var outputFormat string
+	switch {
+	case o.createOptions != nil:
+		outputFormat = o.createOptions.OutputFormat
+	case o.generateOptions != nil:
+		outputFormat = o.generateOptions.OutputFormat
+	default:
+		return fmt.Errorf("unable to determine output format")
+	}
+	var printer printers.ResourcePrinter
+	switch outputFormat {
+	case "yaml":
+		printer = &printers.YAMLPrinter{}
+	case "json":
+		printer = &printers.JSONPrinter{}
+	default:
+		return fmt.Errorf("unsupported output format %q", outputFormat)
+	}
+
+	return printer.PrintObj(pubKey, os.Stdout)
+}
