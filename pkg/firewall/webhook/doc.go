54b78207bb42f2838dd8bda0daef6b6eb494aa9d
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2023-11-15 17:58:34 +0100

    Network: firewall controller chain

--- pkg/firewall/webhook/doc.go
+++ pkg/firewall/webhook/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package webhooks provides a webhook server for the firewall.
-package webhooks
+// Package webhook provides a webhook server for the firewall.
+package webhook
