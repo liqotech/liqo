31de4190af018e0d77d2a2dc1a0d5a53841f47eb
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2023-11-15 17:58:34 +0100

    Network: firewall controller rules

--- pkg/liqo-controller-manager/webhooks/firewallconfiguration/chain.go
+++ pkg/liqo-controller-manager/webhooks/firewallconfiguration/chain.go
@@ -12,13 +12,86 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package webhook
+package firewallconfiguration
 
 import (
+	"fmt"
+
 	"k8s.io/klog/v2"
 
 	firewallapi "github.com/liqotech/liqo/apis/networking/v1alpha1/firewall"
 )
+
+func checkChain(tableFamily firewallapi.TableFamily, chain *firewallapi.Chain) error {
+	if err := checkAllowedTableFamilyChainTypeHook(tableFamily, chain); err != nil {
+		return forgeChainError(chain, err)
+	}
+
+	if err := checkTotalDefinedRulesSets(chain.Rules); err != nil {
+		return forgeChainError(chain, err)
+	}
+
+	if err := allowedChainType(chain.Type, chain.Rules); err != nil {
+		return forgeChainError(chain, err)
+	}
+
+	return nil
+}
+
+func checkAllowedTableFamilyChainTypeHook(tableFamily firewallapi.TableFamily, chain *firewallapi.Chain) error {
+	if !allowedTableFamilyChainTypeHook(tableFamily, *chain.Type, *chain.Hook) {
+		return fmt.Errorf(`in chain %s, the combination of family %s, chain type %s and hook %s is not allowed. 
+		Please refer to https://wiki.nftables.org/wiki-nftables/index.php/Netfilter_hooks`,
+			*chain.Name, tableFamily, *chain.Type, *chain.Hook,
+		)
+	}
+	return nil
+}
+
+func checkTotalDefinedRulesSets(rules firewallapi.RulesSet) error {
+	total := totalDefinedRulesSets(rules)
+	if total == 0 {
+		return fmt.Errorf("no rules set defined")
+	}
+	if total > 1 {
+		return fmt.Errorf("only one rules set must be defined")
+	}
+	return nil
+}
+
+func totalDefinedRulesSets(rules firewallapi.RulesSet) int {
+	total := 0
+	if rules.NatRules != nil {
+		total++
+	}
+	if rules.FilterRules != nil {
+		total++
+	}
+	if rules.RouteRules != nil {
+		total++
+	}
+	return total
+}
+
+func allowedChainType(chaintype *firewallapi.ChainType, rules firewallapi.RulesSet) error {
+	switch *chaintype {
+	case firewallapi.ChainTypeNAT:
+		if rules.NatRules == nil {
+			return fmt.Errorf("NAT rules must be defined when using NAT chain. Please fulfill the rules.natRules field")
+		}
+	case firewallapi.ChainTypeFilter:
+		if rules.FilterRules == nil {
+			return fmt.Errorf("filter rules must be defined when using Filter chain. Please fulfill the rules.filterRules field")
+		}
+	case firewallapi.ChainTypeRoute:
+		if rules.RouteRules == nil {
+			return fmt.Errorf("route rules must be defined when using Route chain. Please fulfill the rules.routeRules field")
+		}
+	default:
+		return fmt.Errorf("chain type not supported")
+	}
+	return nil
+}
 
 // refer to https://wiki.nftables.org/wiki-nftables/index.php/Netfilter_hooks
 func allowedTableFamilyChainTypeHook(familiy firewallapi.TableFamily, chainType firewallapi.ChainType, hook firewallapi.ChainHook) bool {
