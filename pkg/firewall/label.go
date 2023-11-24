f55d8d6e564f83db1f4a5ca8bee18bb620441fed
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2023-11-24 17:23:41 +0100

    Firewall: gateway integration

---
+++ pkg/firewall/label.go
@@ -0,0 +1,22 @@
+// Copyright 2019-2023 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package firewall
+
+const (
+	// FirewallCategoryTargetKey is the key used by the firewallconfiguration controller to reconcile only resources related to a group.
+	FirewallCategoryTargetKey = "liqo.io/firewall/category"
+	// FirewallUniqueTargetKey is the key used by the firewallconfiguration controller to reconcile only resources related to a single component.
+	FirewallUniqueTargetKey = "liqo.io/firewall/unique"
+)
