7301bd4ae5f8131cd9cf99832908722dc2a6436c
Francesco Torta <62566275+fra98@users.noreply.github.com>
2023-12-01 14:40:02 +0100

    Fixed old linter errors

--- apis/sharing/v1alpha1/doc.go
+++ apis/sharing/v1alpha1/doc.go
@@ -12,23 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +kubebuilder:object:generate=true
-// +groupName=networking.liqo.io
-
+// Package v1alpha1 contains API Schema definitions for the sharing v1alpha1 API group
 package v1alpha1
-
-import (
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
-)
-
-var (
-	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "networking.liqo.io", Version: "v1alpha1"}
-
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
-
-	// AddToScheme adds the types in this group-version to the given scheme.
-	AddToScheme = SchemeBuilder.AddToScheme
-)
--- apis/virtualkubelet/v1alpha1/doc.go
+++ apis/virtualkubelet/v1alpha1/doc.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package v1alpha1 contains API Schema definitions for the virtualkubelet v1alpha1 API group.
 // +kubebuilder:object:generate=true
 // +groupName=virtualkubelet.liqo.io
+
+// Package v1alpha1 contains API Schema definitions for the virtualkubelet v1alpha1 API group.
 package v1alpha1
