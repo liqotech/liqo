2fa4442992a8ed28276ff2cce6a94c61e61dc3dd
Alessandro Olivero <aleoli96@gmail.com>
2023-12-18 16:02:56 +0100

    internal network: change internal node api

--- pkg/liqo-controller-manager/internal-network/internalfabric-controller/internalfabric_controller.go
+++ pkg/liqo-controller-manager/internal-network/internalfabric-controller/internalfabric_controller.go
@@ -16,34 +16,19 @@ package internalfabriccontroller
 
 import (
 	"context"
-	"fmt"
-	"slices"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	networkingv1alpha1 "github.com/liqotech/liqo/apis/networking/v1alpha1"
-	"github.com/liqotech/liqo/pkg/consts"
-	"github.com/liqotech/liqo/pkg/discovery"
-	"github.com/liqotech/liqo/pkg/liqo-controller-manager/internal-network/ipam"
 	"github.com/liqotech/liqo/pkg/utils/getters"
 )
-
-var preferredOrder = []corev1.NodeAddressType{
-	corev1.NodeInternalIP,
-	corev1.NodeExternalIP,
-	corev1.NodeInternalDNS,
-	corev1.NodeExternalDNS,
-	corev1.NodeHostName,
-}
 
 // InternalFabricReconciler manage InternalFabric lifecycle.
 type InternalFabricReconciler struct {
@@ -91,37 +76,6 @@ func (r *InternalFabricReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 		}
 	}
 
-	// List all internalnodes
-	var internalNodes networkingv1alpha1.InternalNodeList
-	if err = r.List(ctx, &internalNodes, client.InNamespace(internalFabric.Namespace),
-		client.MatchingLabels{consts.InternalFabricLabelKey: req.Name}); err != nil {
-		klog.Errorf("Unable to list InternalNodes: %s", err)
-		return ctrl.Result{}, err
-	}
-
-	// Get the list of physical nodes names, storing only the ones that are not being deleted.
-	nodeNames := make([]string, len(nodes.Items))
-	for i := range nodes.Items {
-		if nodes.Items[i].GetDeletionTimestamp().IsZero() {
-			nodeNames[i] = nodes.Items[i].Name
-		}
-	}
-
-	// Ensure status
-	internalFabric.Status.AssignedIPs = make(map[string]networkingv1alpha1.IP)
-	for i := range internalNodes.Items {
-		internalNode := &internalNodes.Items[i]
-		// Delete InternalNode if the associated physical node is deleted/deleting.
-		if !slices.Contains(nodeNames, internalNode.Name) {
-			if err := r.deleteInternalNode(ctx, internalNode); err != nil {
-				klog.Errorf("Unable to delete InternalNode %q: %s", internalNode.Name, err)
-				return ctrl.Result{}, err
-			}
-		} else {
-			internalFabric.Status.AssignedIPs[internalNode.Name] = internalNode.Spec.IP
-		}
-	}
-
 	if err = r.Status().Update(ctx, internalFabric); err != nil {
 		klog.Errorf("Unable to update InternalFabric status: %s", err)
 		return ctrl.Result{}, err
@@ -132,67 +86,6 @@ func (r *InternalFabricReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 
 func (r *InternalFabricReconciler) reconcileNode(ctx context.Context,
 	internalFabric *networkingv1alpha1.InternalFabric, node *corev1.Node) error {
-	internalNode := &networkingv1alpha1.InternalNode{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      node.Name,
-			Namespace: internalFabric.Namespace,
-		},
-	}
-
-	if !node.GetDeletionTimestamp().IsZero() {
-		// Do not create/update internalNode if the associated physical node is being deleted.
-		// Note: deletion of the internalNode will be handled after.
-		return nil
-	}
-
-	if _, err := controllerutil.CreateOrUpdate(ctx, r.Client, internalNode, func() error {
-		if internalNode.Labels == nil {
-			internalNode.Labels = make(map[string]string)
-		}
-		internalNode.Labels[consts.InternalFabricLabelKey] = internalFabric.Name
-
-		internalNode.Spec.FabricRef = &corev1.ObjectReference{
-			Name:      internalFabric.Name,
-			Namespace: internalFabric.Namespace,
-			UID:       internalFabric.UID,
-		}
-
-		intIPAM, err := ipam.GetNodeIpam(ctx, r.Client)
-		if err != nil {
-			return err
-		}
-
-		ip, err := intIPAM.Allocate(fmt.Sprintf("%s/%s/%s",
-			internalFabric.Namespace, internalFabric.Name, internalNode.Name))
-		if err != nil {
-			return err
-		}
-
-		internalNode.Spec.IP = networkingv1alpha1.IP(ip.String())
-		internalNode.Spec.NodeAddr, err = discovery.GetAddressFromNodeListWithPreferredOrder([]corev1.Node{*node}, preferredOrder)
-		if err != nil {
-			return err
-		}
-		internalNode.Spec.IsGateway = node.Name == internalFabric.Spec.NodeName
-
-		return controllerutil.SetControllerReference(internalFabric, internalNode, r.Scheme)
-	}); err != nil {
-		klog.Errorf("Unable to create or update InternalNode %q: %s", internalNode.Name, err)
-		return err
-	}
-	return nil
-}
-
-func (r *InternalFabricReconciler) deleteInternalNode(ctx context.Context, internalNode *networkingv1alpha1.InternalNode) error {
-	if err := r.Delete(ctx, internalNode); err != nil {
-		if apierrors.IsNotFound(err) {
-			klog.Infof("InternalNode %q not found", internalNode.Name)
-			return nil
-		}
-		klog.Errorf("Unable to delete InternalNode %q: %s", internalNode.Name, err)
-		return err
-	}
-	klog.Infof("InternalNode %q deleted as associated physical node does not exists anymore", internalNode.Name)
 	return nil
 }
 
@@ -220,7 +113,6 @@ func (r *InternalFabricReconciler) SetupWithManager(mgr ctrl.Manager) error {
 
 	return ctrl.NewControllerManagedBy(mgr).
 		Watches(&corev1.Node{}, nodeEnqueuer).
-		Owns(&networkingv1alpha1.InternalNode{}).
 		For(&networkingv1alpha1.InternalFabric{}).
 		Complete(r)
 }
