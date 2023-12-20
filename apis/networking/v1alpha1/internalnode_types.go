9001a7fd65cbb5e2eb5e8cae616b08fe8739d9cd
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2023-12-20 17:35:11 +0100

    Network: Internal API

--- apis/networking/v1alpha1/internalnode_types.go
+++ apis/networking/v1alpha1/internalnode_types.go
@@ -15,7 +15,6 @@
 package v1alpha1
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -35,59 +34,39 @@ var InternalNodeGroupResource = schema.GroupResource{Group: GroupVersion.Group, 
 // InternalNodeGroupVersionResource is groupResourceVersion used to register these objects.
 var InternalNodeGroupVersionResource = GroupVersion.WithResource(InternalNodeResource)
 
+// InternalNodeSpecInterfaceGateway contains the information about the gateway interface.
+type InternalNodeSpecInterfaceGateway struct {
+	// Name is the name of the interface added to the gateways.
+	Name string `json:"name"`
+}
+
+// InternalNodeSpecInterface contains the information about network interfaces.
+type InternalNodeSpecInterface struct {
+	// Gateway contains the information about the gateway interface.
+	// The gateway interface is created on every gateway to connect them to the node related with the internalnode.
+	Gateway InternalNodeSpecInterfaceGateway `json:"gateway"`
+}
+
 // InternalNodeSpec defines the desired state of InternalNode.
 type InternalNodeSpec struct {
-	// IP is the IP address to assign to the internal interface.
-	IP IP `json:"ip,omitempty"`
-	// NodeAddr is the address of the node.
-	NodeAddr string `json:"nodeAddr,omitempty"`
-}
-
-// InternalNodeConditionType is a valid value for InternalNodeCondition.Type.
-type InternalNodeConditionType string
-
-const (
-	// InternalNodeConditionTypeApplied means the route has been applied.
-	InternalNodeConditionTypeApplied InternalNodeConditionType = "Applied"
-)
-
-// InternalNodeCondition contains details for the current condition of this node.
-type InternalNodeCondition struct {
-	// Type is the type of the condition.
-	// +kubebuilder:validation:Enum=Applied
-	Type InternalNodeConditionType `json:"type"`
-	// Status is the status of the condition.
-	// +kubebuilder:validation:Enum=True;False;Unknown
-	// +kubebuilder:default=Unknown
-	Status corev1.ConditionStatus `json:"status"`
-	// LastTransitionTime is the last time the condition transitioned from one status to another.
-	LastTransitionTime metav1.Time `json:"lastTransitionTime"`
-	// Reason is a unique, one-word, CamelCase reason for the condition's last transition.
-	Reason string `json:"reason,omitempty"`
-	// Message is a human-readable message indicating details about last transition.
-	Message string `json:"message,omitempty"`
-}
-
-// InternalNodeStatus defines the observed state of InternalNode.
-type InternalNodeStatus struct {
-	// Conditions contains information about the current status of the node.
-	Conditions []InternalNodeCondition `json:"conditions,omitempty"`
+	// Interface contains the information about network interfaces.
+	Interface InternalNodeSpecInterface `json:"interface"`
+	// NodeAddress is the address of the node.
+	NodeAddress string `json:"nodeAddress"`
 }
 
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:scope=Cluster,categories=liqo
-// +kubebuilder:subresource:status
-// +kubebuilder:printcolumn:name="Status",type=string,JSONPath=`.status.conditions[?(@.type == 'Applied')].status`
+// +kubebuilder:printcolumn:name="Node Address",type=string,JSONPath=`.spec.nodeAddress`
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
 
-// InternalNode contains the network internalnode of a pair of clusters,
-// including the local and the remote pod and external CIDRs and how the where remapped.
+// InternalNode contains the network internalnode settings.
+// Every internalnode resource represents a node in the local cluster.
 type InternalNode struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   InternalNodeSpec   `json:"spec,omitempty"`
-	Status InternalNodeStatus `json:"status,omitempty"`
+	Spec InternalNodeSpec `json:"spec,omitempty"`
 }
 
 // +kubebuilder:object:root=true
