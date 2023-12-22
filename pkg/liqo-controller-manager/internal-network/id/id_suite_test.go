9f813af6f160c11a38d9e25715c8b8ba5c2e7026
Alessandro Olivero <aleoli96@gmail.com>
2023-12-22 12:22:12 +0100

    geneve tunnel operator

---
+++ pkg/liqo-controller-manager/internal-network/id/id_suite_test.go
@@ -0,0 +1,42 @@
+// Copyright 2019-2023 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package id
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/liqotech/liqo/pkg/utils/testutil"
+)
+
+var (
+	ctx    context.Context
+	cancel context.CancelFunc
+)
+
+func TestID(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ID Suite")
+}
+
+var _ = BeforeSuite(func() {
+	testutil.LogsToGinkgoWriter()
+})
+
+var _ = BeforeEach(func() { ctx, cancel = context.WithCancel(context.Background()) })
+var _ = AfterEach(func() { cancel() })
