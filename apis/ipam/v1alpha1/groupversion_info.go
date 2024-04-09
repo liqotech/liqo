58be3422483437d7a67248696c20f99e05a27e32
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2024-04-09 09:36:24 +0200

    Generate clients for IP resource

--- apis/ipam/v1alpha1/groupversion_info.go
+++ apis/ipam/v1alpha1/groupversion_info.go
@@ -23,12 +23,17 @@ import (
 )
 
 var (
-	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "ipam.liqo.io", Version: "v1alpha1"}
+	// SchemeGroupVersion is group version used to register these objects.
+	SchemeGroupVersion = schema.GroupVersion{Group: "ipam.liqo.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource.
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
