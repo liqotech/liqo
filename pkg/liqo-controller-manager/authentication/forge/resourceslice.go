606dc2a3aa2cbdf45f7b2d459d24b0883d222cb1
Alessandro Olivero <aleoli96@gmail.com>
2024-04-09 15:42:37 +0200

    handle resourceslice lifecycle

---
+++ pkg/liqo-controller-manager/authentication/forge/resourceslice.go
@@ -0,0 +1,83 @@
+// Copyright 2019-2024 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package forge
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	authv1alpha1 "github.com/liqotech/liqo/apis/authentication/v1alpha1"
+	"github.com/liqotech/liqo/pkg/consts"
+)
+
+// ResourceSliceOptions contains the options to forge a ResourceSlice resource.
+type ResourceSliceOptions struct {
+	Class     authv1alpha1.ResourceSliceClass
+	Resources map[corev1.ResourceName]string
+}
+
+// ResourceSlice forges a ResourceSlice resource.
+func ResourceSlice(name, namespace string) *authv1alpha1.ResourceSlice {
+	return &authv1alpha1.ResourceSlice{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: authv1alpha1.GroupVersion.String(),
+			Kind:       authv1alpha1.ResourceSliceKind,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+}
+
+// MutateResourceSlice mutates a ResourceSlice resource.
+func MutateResourceSlice(resourceSlice *authv1alpha1.ResourceSlice, remoteClusterID string,
+	opts *ResourceSliceOptions) error {
+	if resourceSlice.Labels == nil {
+		resourceSlice.Labels = map[string]string{}
+	}
+	resourceSlice.Labels[consts.ReplicationRequestedLabel] = consts.ReplicationRequestedLabelValue
+	resourceSlice.Labels[consts.ReplicationDestinationLabel] = remoteClusterID
+	resourceSlice.Labels[consts.RemoteClusterID] = remoteClusterID
+
+	rl, err := resourceList(opts.Resources)
+	if err != nil {
+		return err
+	}
+
+	resourceSlice.Spec = authv1alpha1.ResourceSliceSpec{
+		Class:     opts.Class,
+		Resources: rl,
+	}
+	return nil
+}
+
+func resourceList(resources map[corev1.ResourceName]string) (corev1.ResourceList, error) {
+	resourceList := corev1.ResourceList{}
+	for name, quantity := range resources {
+		if quantity == "" {
+			continue
+		}
+
+		qnt, err := resource.ParseQuantity(quantity)
+		if err != nil {
+			return nil, err
+		}
+		resourceList[name] = qnt
+	}
+
+	return resourceList, nil
+}
