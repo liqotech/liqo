3554083ef79d5683a5275d735892def04b8c5d5c
Alessandro Olivero <aleoli96@gmail.com>
2024-04-09 15:42:37 +0200

    handle resourceslice lifecycle

---
+++ pkg/liqo-controller-manager/authentication/localresourceslice-controller/localresourceslice_controller.go
@@ -0,0 +1,158 @@
+// Copyright 2019-2024 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package localresourceslicecontroller
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+	klog "k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	authv1alpha1 "github.com/liqotech/liqo/apis/authentication/v1alpha1"
+	discoveryv1alpha1 "github.com/liqotech/liqo/apis/discovery/v1alpha1"
+	"github.com/liqotech/liqo/internal/crdReplicator/reflection"
+	"github.com/liqotech/liqo/pkg/liqo-controller-manager/authentication"
+	tenantnamespace "github.com/liqotech/liqo/pkg/tenantNamespace"
+	"github.com/liqotech/liqo/pkg/utils/getters"
+)
+
+// NewLocalResourceSliceReconciler returns a new LocalResourceSliceReconciler.
+func NewLocalResourceSliceReconciler(cl client.Client, s *runtime.Scheme,
+	recorder record.EventRecorder, liqoNamespace string,
+	localClusterIdentity *discoveryv1alpha1.ClusterIdentity) *LocalResourceSliceReconciler {
+	return &LocalResourceSliceReconciler{
+		Client: cl,
+		Scheme: s,
+
+		eventRecorder: recorder,
+
+		liqoNamespace:        liqoNamespace,
+		localClusterIdentity: localClusterIdentity,
+	}
+}
+
+// LocalResourceSliceReconciler reconciles a ResourceSliceReconciler object.
+type LocalResourceSliceReconciler struct {
+	client.Client
+	*runtime.Scheme
+
+	eventRecorder record.EventRecorder
+
+	liqoNamespace        string
+	localClusterIdentity *discoveryv1alpha1.ClusterIdentity
+}
+
+// cluster-role
+// +kubebuilder:rbac:groups=authentication.liqo.io,resources=resourceslices;resourceslices/status,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=authentication.liqo.io,resources=identities,verbs=get;list;watch
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch
+
+// Reconcile local ResourceSlice resources.
+func (r *LocalResourceSliceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	var resourceSlice authv1alpha1.ResourceSlice
+	if err := r.Get(ctx, req.NamespacedName, &resourceSlice); err != nil {
+		if errors.IsNotFound(err) {
+			klog.V(4).Infof("resourceSlice %q not found", req.NamespacedName)
+			return ctrl.Result{}, nil
+		}
+		klog.Errorf("unable to get ResourceSlice %q: %v", req.NamespacedName, err)
+		return ctrl.Result{}, err
+	}
+
+	if resourceSlice.Spec.ConsumerClusterIdentity == nil {
+		// Set the identity of the local cluster as the consumer identity.
+		resourceSlice.Spec.ConsumerClusterIdentity = r.localClusterIdentity.DeepCopy()
+	}
+
+	if resourceSlice.Spec.ProviderClusterIdentity == nil {
+		// Get the identity from the Tenant.
+		tenantNamespace := resourceSlice.Namespace
+		var ns corev1.Namespace
+		if err := r.Get(ctx, client.ObjectKey{Name: tenantNamespace}, &ns); err != nil {
+			klog.Errorf("unable to get Namespace %q: %v", tenantNamespace, err)
+			r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "FailedGetNamespace", err.Error())
+			return ctrl.Result{}, err
+		}
+
+		clusterID, err := tenantnamespace.GetClusterIDFromTenantNamespace(&ns)
+		if err != nil {
+			klog.Errorf("unable to get ClusterID from Namespace %q: %v", tenantNamespace, err)
+			r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "FailedGetClusterID", err.Error())
+			return ctrl.Result{}, err
+		}
+
+		identity, err := getters.GetControlPlaneIdentityByClusterID(ctx, r.Client, clusterID)
+		if err != nil {
+			klog.Errorf("unable to get Control Plane Identity by ClusterID %q: %v", clusterID, err)
+			r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "FailedGetControlPlaneIdentity", err.Error())
+			return ctrl.Result{}, err
+		}
+
+		resourceSlice.Spec.ProviderClusterIdentity = identity.Spec.ClusterIdentity.DeepCopy()
+	}
+
+	// Get public and private keys of the local cluster.
+	privateKey, _, err := authentication.GetClusterKeys(ctx, r.Client, r.liqoNamespace)
+	if err != nil {
+		klog.Errorf("unable to get local cluster keys: %v", err)
+		r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "FailedGetLocalClusterKeys", err.Error())
+		return ctrl.Result{}, err
+	}
+
+	if resourceSlice.Spec.CSR == nil || len(resourceSlice.Spec.CSR) == 0 {
+		// Generate a CSR for the remote cluster.
+		CSR, err := authentication.GenerateCSRForResourceSlice(privateKey, &resourceSlice)
+		if err != nil {
+			klog.Errorf("unable to generate CSR for ResourceSlice %q: %v", req.NamespacedName, err)
+			r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "FailedGenerateCSR", err.Error())
+			return ctrl.Result{}, err
+		}
+
+		resourceSlice.Spec.CSR = CSR
+	}
+
+	if err = r.Update(ctx, &resourceSlice); err != nil {
+		klog.Errorf("unable to update ResourceSlice %q: %v", req.NamespacedName, err)
+		r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "FailedUpdateResourceSlice", err.Error())
+		return ctrl.Result{}, err
+	}
+
+	klog.Infof("ResourceSlice %q reconciled", req.NamespacedName)
+	r.eventRecorder.Event(&resourceSlice, corev1.EventTypeNormal, "Reconciled", "ResourceSlice reconciled")
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *LocalResourceSliceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// generate the predicate to filter just the ResourceSlices created by the local cluster checking crdReplicator labels
+	p, err := predicate.LabelSelectorPredicate(reflection.LocalResourcesLabelSelector())
+	if err != nil {
+		klog.Error(err)
+		return err
+	}
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&authv1alpha1.ResourceSlice{}, builder.WithPredicates(p)).
+		Complete(r)
+}
---
+++ pkg/liqo-controller-manager/authentication/remoteresourceslice-controller/remoteresourceslice_controller.go
@@ -0,0 +1,244 @@
+// Copyright 2019-2024 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package remoteresourceslicecontroller
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
+	klog "k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	authv1alpha1 "github.com/liqotech/liqo/apis/authentication/v1alpha1"
+	"github.com/liqotech/liqo/internal/crdReplicator/reflection"
+	identitymanager "github.com/liqotech/liqo/pkg/identityManager"
+	"github.com/liqotech/liqo/pkg/liqo-controller-manager/authentication"
+	"github.com/liqotech/liqo/pkg/utils/apiserver"
+)
+
+// NewRemoteResourceSliceReconciler returns a new RemoteResourceSliceReconciler.
+func NewRemoteResourceSliceReconciler(cl client.Client, s *runtime.Scheme, config *rest.Config,
+	recorder record.EventRecorder, identityProvider identitymanager.IdentityProvider,
+	apiServerAddressOverride string, caOverride []byte, trustedCA bool) *RemoteResourceSliceReconciler {
+	return &RemoteResourceSliceReconciler{
+		Client: cl,
+		Scheme: s,
+		Config: config,
+
+		eventRecorder:    recorder,
+		identityProvider: identityProvider,
+
+		apiServerAddressOverride: apiServerAddressOverride,
+		caOverride:               caOverride,
+		trustedCA:                trustedCA,
+
+		reconciledClasses: []authv1alpha1.ResourceSliceClass{
+			authv1alpha1.ResourceSliceClassDefault,
+			authv1alpha1.ResourceSliceClassUnknown,
+		},
+	}
+}
+
+// RemoteResourceSliceReconciler reconciles a ResourceSliceReconciler object.
+type RemoteResourceSliceReconciler struct {
+	client.Client
+	*runtime.Scheme
+	Config *rest.Config
+
+	eventRecorder    record.EventRecorder
+	identityProvider identitymanager.IdentityProvider
+
+	apiServerAddressOverride string
+	caOverride               []byte
+	trustedCA                bool
+
+	reconciledClasses []authv1alpha1.ResourceSliceClass
+}
+
+// cluster-role
+// +kubebuilder:rbac:groups=authentication.liqo.io,resources=resourceslices;resourceslices/status,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile replicated ResourceSlice resources.
+func (r *RemoteResourceSliceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (res ctrl.Result, err error) {
+	var resourceSlice authv1alpha1.ResourceSlice
+	if err = r.Get(ctx, req.NamespacedName, &resourceSlice); err != nil {
+		if errors.IsNotFound(err) {
+			klog.V(4).Infof("resourceSlice %q not found", req.NamespacedName)
+			return ctrl.Result{}, nil
+		}
+		klog.Errorf("unable to get ResourceSlice %q: %v", req.NamespacedName, err)
+		return ctrl.Result{}, err
+	}
+
+	defer func() {
+		errDef := r.Client.Status().Update(ctx, &resourceSlice)
+		if errDef != nil {
+			klog.Errorf("Unable to update the ResourceSlice %q: %s", req.NamespacedName, errDef)
+			r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "UpdateFailed", errDef.Error())
+			err = errDef
+		}
+
+		if err == nil {
+			r.eventRecorder.Event(&resourceSlice, corev1.EventTypeNormal, "Reconciled", "ResourceSlice reconciled")
+		}
+	}()
+
+	if isInResourceClasses(&resourceSlice, r.reconciledClasses...) {
+		// TODO: compute the resources
+		findOrDefault := func(resource corev1.ResourceName, val resource.Quantity) resource.Quantity {
+			if _, ok := resourceSlice.Spec.Resources[resource]; !ok {
+				return val
+			}
+			return resourceSlice.Spec.Resources[resource]
+		}
+		resourceSlice.Status.Resources = corev1.ResourceList{
+			corev1.ResourceCPU:    findOrDefault(corev1.ResourceCPU, resource.MustParse("2")),
+			corev1.ResourceMemory: findOrDefault(corev1.ResourceMemory, resource.MustParse("4Gi")),
+			corev1.ResourcePods:   findOrDefault(corev1.ResourcePods, resource.MustParse("100")),
+		}
+
+		acceptResources(&resourceSlice, r.eventRecorder)
+	}
+
+	// forge the AuthParams
+	if resourceSlice.Spec.ConsumerClusterIdentity == nil {
+		err = fmt.Errorf("ConsumerClusterIdentity not set")
+		klog.Errorf("Unable to ensure the remote certificate for the ResourceSlice %q: %s", req.NamespacedName, err)
+		r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "RemoteCertificateFailed", err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	resp, err := identitymanager.EnsureCertificate(ctx, r.identityProvider, &identitymanager.SigningRequestOptions{
+		Cluster:        resourceSlice.Spec.ConsumerClusterIdentity,
+		Namespace:      resourceSlice.Namespace,
+		IdentityType:   authv1alpha1.VirtualNodeIdentityType,
+		Name:           resourceSlice.Name,
+		SigningRequest: resourceSlice.Spec.CSR,
+	})
+	if err != nil {
+		klog.Errorf("Unable to ensure the remote certificate for the ResourceSlice %q: %s", req.NamespacedName, err)
+		r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "RemoteCertificateFailed", err.Error())
+		return ctrl.Result{}, err
+	}
+
+	apiServer, err := apiserver.GetURL(ctx, r.apiServerAddressOverride, r.Client)
+	if err != nil {
+		klog.Errorf("Unable to get the API server URL: %s", err)
+		r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "APIServerURLFailed", err.Error())
+		return ctrl.Result{}, err
+	}
+
+	ca, err := apiserver.RetrieveAPIServerCA(r.Config, r.caOverride, r.trustedCA)
+	if err != nil {
+		klog.Errorf("Unable to get the API server CA: %s", err)
+		r.eventRecorder.Event(&resourceSlice, corev1.EventTypeWarning, "APIServerCAFailed", err.Error())
+		return ctrl.Result{}, err
+	}
+
+	resourceSlice.Status.AuthParams = r.identityProvider.ForgeAuthParams(resp, apiServer, ca)
+
+	// accept the ResourceSlice
+	acceptResourceSlice(&resourceSlice, r.eventRecorder)
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *RemoteResourceSliceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// generate the predicate to filter just the ResourceSlices created by the remote cluster checking crdReplicator labels
+	p, err := predicate.LabelSelectorPredicate(reflection.ReplicatedResourcesLabelSelector())
+	if err != nil {
+		klog.Error(err)
+		return err
+	}
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&authv1alpha1.ResourceSlice{},
+			builder.WithPredicates(p),
+			withCSR()).
+		Complete(r)
+}
+
+func acceptResourceSlice(resourceSlice *authv1alpha1.ResourceSlice, er record.EventRecorder) {
+	switch authentication.EnsureCondition(
+		resourceSlice,
+		authv1alpha1.ResourceSliceConditionTypeAuthentication,
+		authv1alpha1.ResourceSliceConditionAccepted,
+		"ResourceSliceAuthenticationAccepted",
+		"ResourceSlice authentication accepted",
+	) {
+	case controllerutil.OperationResultNone:
+		klog.V(4).Infof("ResourceSlice authentication %q already accepted", resourceSlice.Name)
+	case controllerutil.OperationResultUpdated:
+		klog.Infof("ResourceSlice authentication %q confirmed", resourceSlice.Name)
+		er.Event(resourceSlice, corev1.EventTypeNormal, "ResourceSliceAuthenticationAccepted", "ResourceSlice authentication confirmed")
+	case controllerutil.OperationResultCreated:
+		klog.Infof("ResourceSlice authentication %q accepted", resourceSlice.Name)
+		er.Event(resourceSlice, corev1.EventTypeNormal, "ResourceSliceAuthenticationAccepted", "ResourceSlice authentication accepted")
+	default:
+		return
+	}
+}
+
+func isInResourceClasses(resourceSlice *authv1alpha1.ResourceSlice, classes ...authv1alpha1.ResourceSliceClass) bool {
+	for _, class := range classes {
+		if resourceSlice.Spec.Class == class {
+			return true
+		}
+	}
+	return false
+}
+
+func acceptResources(resourceSlice *authv1alpha1.ResourceSlice, er record.EventRecorder) {
+	switch authentication.EnsureCondition(
+		resourceSlice,
+		authv1alpha1.ResourceSliceConditionTypeResources,
+		authv1alpha1.ResourceSliceConditionAccepted,
+		"ResourceSliceResourcesAccepted",
+		"ResourceSlice resources accepted",
+	) {
+	case controllerutil.OperationResultNone:
+		klog.V(4).Infof("ResourceSlice resources %q already accepted", resourceSlice.Name)
+	case controllerutil.OperationResultUpdated:
+		klog.Infof("Resources %q updated", resourceSlice.Name)
+		er.Event(resourceSlice, corev1.EventTypeNormal, "ResourceSliceResourcesAccepted", "ResourceSlice resources updated")
+	case controllerutil.OperationResultCreated:
+		klog.Infof("Resources %q accepted", resourceSlice.Name)
+		er.Event(resourceSlice, corev1.EventTypeNormal, "ResourceSliceResourcesAccepted", "ResourceSlice resources accepted")
+	default:
+		return
+	}
+}
+
+func withCSR() builder.ForOption {
+	return builder.WithPredicates(predicate.NewPredicateFuncs(func(obj client.Object) bool {
+		rs, ok := obj.(*authv1alpha1.ResourceSlice)
+		if !ok {
+			return false
+		}
+		return rs.Spec.CSR != nil && len(rs.Spec.CSR) > 0
+	}))
+}
