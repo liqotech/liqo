bd77fb5cbcf5413ba1b613057ee964e3fc2a9bc7
Alessandro Olivero <aleoli96@gmail.com>
2024-04-23 15:35:53 +0200

    refactor iam auth

--- pkg/liqo-controller-manager/foreign-cluster-operator/foreign-cluster-operator_test.go
+++ pkg/liqo-controller-manager/foreign-cluster-operator/foreign-cluster-operator_test.go
@@ -135,7 +135,8 @@ var _ = Describe("ForeignClusterOperator", func() {
 		Expect(authSvc).ToNot(BeNil())
 
 		namespaceManager := tenantnamespace.NewManager(cluster.GetClient())
-		identityManagerCtrl := identitymanager.NewCertificateIdentityManager(mgr.GetClient(), cluster.GetClient(), homeCluster, namespaceManager)
+		identityManagerCtrl := identitymanager.NewCertificateIdentityManager(ctx, mgr.GetClient(), cluster.GetClient(), mgr.GetConfig(),
+			homeCluster, namespaceManager)
 
 		foreignCluster := discoveryv1alpha1.ClusterIdentity{
 			ClusterID:   "foreign-cluster-id",
--- pkg/liqoctl/status/local/localinfo.go
+++ pkg/liqoctl/status/local/localinfo.go
@@ -195,7 +195,7 @@ func (lic *LocalInfoChecker) addEndpointsSection(ctx context.Context) {
 	}
 	// ExtractvalueFromArgumentList errors are not handled because GetURL is able to handle void values.
 	apiServerAddressArg, _ := liqoctlutils.ExtractValuesFromArgumentList("--advertise-api-server-address", authargs)
-	apiServerAddress, err := apiserver.GetURL(ctx, apiServerAddressArg, lic.options.CRClient)
+	apiServerAddress, err := apiserver.GetURL(ctx, lic.options.CRClient, apiServerAddressArg)
 	if err != nil {
 		lic.addCollectionError(fmt.Errorf("unable to get api server address: %w", err))
 	} else {
