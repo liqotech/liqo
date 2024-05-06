ff3425107d50188f8126b88a973d847e139dc744
Alessandro Olivero <aleoli96@gmail.com>
2024-05-06 12:42:48 +0200

    fix unit test

--- pkg/utils/testutil/cluster.go
+++ pkg/utils/testutil/cluster.go
@@ -27,6 +27,7 @@ import (
 
 	discoveryv1alpha1 "github.com/liqotech/liqo/apis/discovery/v1alpha1"
 	netv1alpha1 "github.com/liqotech/liqo/apis/net/v1alpha1"
+	networkingv1alpha1 "github.com/liqotech/liqo/apis/networking/v1alpha1"
 	sharingv1alpha1 "github.com/liqotech/liqo/apis/sharing/v1alpha1"
 	virtualkubeletv1alpha1 "github.com/liqotech/liqo/apis/virtualkubelet/v1alpha1"
 )
@@ -74,6 +75,7 @@ func NewTestCluster(crdPath []string) (Cluster, manager.Manager, error) {
 	utilruntime.Must(sharingv1alpha1.AddToScheme(scheme.Scheme))
 	utilruntime.Must(netv1alpha1.AddToScheme(scheme.Scheme))
 	utilruntime.Must(virtualkubeletv1alpha1.AddToScheme(scheme.Scheme))
+	utilruntime.Must(networkingv1alpha1.AddToScheme(scheme.Scheme))
 
 	mgr, err := ctrl.NewManager(cluster.cfg, ctrl.Options{
 		Scheme:  scheme.Scheme,
