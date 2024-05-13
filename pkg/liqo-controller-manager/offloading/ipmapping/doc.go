7d906cec14d33d10d3f70b3cce5bd823826266ef
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2024-05-13 12:23:10 +0200

    Network: leaf to leaf from external source

--- pkg/liqo-controller-manager/offloading/ipmapping/doc.go
+++ pkg/liqo-controller-manager/offloading/ipmapping/doc.go
@@ -12,23 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package offloadedpodcontroller
-
-import (
-	corev1 "k8s.io/api/core/v1"
-
-	"github.com/liqotech/liqo/pkg/liqo-controller-manager/external-network/remapping"
-)
-
-const (
-	offloadedPodNameLabelKey      = "offloading.liqo.io/pod-name"
-	offloadedPodNamespaceLabelKey = "offloading.liqo.io/pod-namespace"
-)
-
-func forgeIPLabels(pod *corev1.Pod) map[string]string {
-	return map[string]string{
-		remapping.IPCategoryTargetKey: remapping.IPCategoryTargetValueMapping,
-		offloadedPodNameLabelKey:      pod.Name,
-		offloadedPodNamespaceLabelKey: pod.Namespace,
-	}
-}
+// Package ipmapping contains the logic to connect the network moduler with the offloading one.
+package ipmapping
