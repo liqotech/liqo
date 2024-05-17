78364784520b7d84e2a26f84038917a2db8c7d92
Francesco Torta <62566275+fra98@users.noreply.github.com>
2024-05-17 17:33:42 +0200

    Cordon Tenant

---
+++ cmd/liqoctl/cmd/cordon.go
@@ -0,0 +1,79 @@
+// Copyright 2019-2024 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/liqotech/liqo/pkg/liqoctl/completion"
+	"github.com/liqotech/liqo/pkg/liqoctl/cordon"
+	"github.com/liqotech/liqo/pkg/liqoctl/factory"
+	"github.com/liqotech/liqo/pkg/liqoctl/output"
+)
+
+const liqoctlCordonTenantLongHelp = `Cordon a tenant cluster.
+
+This command allows to cordon a tenant cluster, preventing it from receiving new resources.
+Resources provided by existing ResourceSlices are left untouched, while new ResourceSlices
+are denied.
+
+Examples:
+  $ {{ .Executable }} cordon tenant my-tenant-name
+`
+
+// newCordonCommand represents the cordon command.
+func newCordonCommand(ctx context.Context, f *factory.Factory) *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "cordon",
+		Short: "Cordon a liqo resource",
+		Long:  "Cordon a liqo resource",
+		Args:  cobra.NoArgs,
+	}
+
+	cmd.AddCommand(newCordonTenantCommand(ctx, f))
+
+	return cmd
+}
+
+// newCordonTenantCommand represents the cordon command.
+func newCordonTenantCommand(ctx context.Context, f *factory.Factory) *cobra.Command {
+	options := cordon.NewOptions(f)
+
+	var cmd = &cobra.Command{
+		Use:               "tenant",
+		Short:             "Cordon a tenant cluster",
+		Long:              WithTemplate(liqoctlCordonTenantLongHelp),
+		Args:              cobra.ExactArgs(1),
+		ValidArgsFunction: completion.Tenants(ctx, f, 1),
+
+		PreRun: func(cmd *cobra.Command, args []string) {
+			output.ExitOnErr(options.Printer.AskConfirm("cordon", options.SkipConfirm))
+		},
+
+		Run: func(cmd *cobra.Command, args []string) {
+			options.TenantName = args[0]
+			output.ExitOnErr(options.RunCordonTenant(ctx))
+		},
+	}
+
+	options.Factory.AddFlags(cmd.PersistentFlags(), cmd.RegisterFlagCompletionFunc)
+
+	cmd.PersistentFlags().DurationVar(&options.Timeout, "timeout", 120*time.Second, "Timeout for cordon completion")
+
+	return cmd
+}
---
+++ pkg/liqoctl/uncordon/handler.go
@@ -0,0 +1,65 @@
+// Copyright 2019-2024 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package uncordon
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	authv1alpha1 "github.com/liqotech/liqo/apis/authentication/v1alpha1"
+	"github.com/liqotech/liqo/pkg/liqoctl/factory"
+	"github.com/liqotech/liqo/pkg/liqoctl/output"
+)
+
+// Options encapsulates the arguments of the uncordon command.
+type Options struct {
+	*factory.Factory
+
+	TenantName string
+
+	Timeout time.Duration
+}
+
+// NewOptions returns a new Options struct.
+func NewOptions(f *factory.Factory) *Options {
+	return &Options{
+		Factory: f,
+	}
+}
+
+// RunUncordonTenant uncordons a tenant cluster.
+func (o *Options) RunUncordonTenant(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, o.Timeout)
+	defer cancel()
+
+	var tenant authv1alpha1.Tenant
+	if err := o.CRClient.Get(ctx, client.ObjectKey{Name: o.TenantName}, &tenant); err != nil {
+		o.Printer.CheckErr(fmt.Errorf("unable to get tenant: %v", output.PrettyErr(err)))
+		return err
+	}
+
+	tenant.Spec.TenantCondition = authv1alpha1.TenantConditionActive
+	if err := o.CRClient.Update(ctx, &tenant); err != nil {
+		o.Printer.CheckErr(fmt.Errorf("unable to update tenant: %v", output.PrettyErr(err)))
+		return err
+	}
+
+	o.Printer.Success.Printfln("Tenant %q uncordoned", o.TenantName)
+
+	return nil
+}
