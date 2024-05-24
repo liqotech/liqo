216d57bb90609002b4daa1ffbe5a3a4ab6fff0db
Francesco Torta <62566275+fra98@users.noreply.github.com>
2024-05-24 15:34:36 +0200

    Refactor API ForeignCluster

--- apis/discovery/v1alpha1/foreigncluster_types.go
+++ apis/discovery/v1alpha1/foreigncluster_types.go
@@ -18,103 +18,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// PeeringConditionStatusType indicates the phase of a peering with a remote cluster.
-type PeeringConditionStatusType string
-
-const (
-	// PeeringConditionStatusNone indicates that there is no peering.
-	PeeringConditionStatusNone PeeringConditionStatusType = "None"
-	// PeeringConditionStatusPending indicates that the peering is pending,
-	// and we are either waiting for the remote cluster feedback or for us to accept the ResourceOffer.
-	PeeringConditionStatusPending PeeringConditionStatusType = "Pending"
-	// PeeringConditionStatusEstablished indicates that the peering has been established.
-	PeeringConditionStatusEstablished PeeringConditionStatusType = "Established"
-	// PeeringConditionStatusDisconnecting indicates that the peering is being deleted.
-	PeeringConditionStatusDisconnecting PeeringConditionStatusType = "Disconnecting"
-	// PeeringConditionStatusDenied indicates that the condition has been denied.
-	// This is only used by the AuthenticationCondition Type, and indicates that
-	// the authentication has been denied even if we provided a token.
-	PeeringConditionStatusDenied PeeringConditionStatusType = "Denied"
-	// PeeringConditionStatusEmptyDenied indicates that the condition has been denied.
-	// This is only used by the AuthenticationCondition Type, and indicates that
-	// the identity verification was denied with an empty token.
-	PeeringConditionStatusEmptyDenied PeeringConditionStatusType = "EmptyDenied"
-	// PeeringConditionStatusError indicates that an error has occurred.
-	PeeringConditionStatusError PeeringConditionStatusType = "Error"
-	// PeeringConditionStatusSuccess indicates that the condition is successful.
-	PeeringConditionStatusSuccess PeeringConditionStatusType = "Success"
-	// PeeringConditionStatusDisabled indicates that the condition is disabled or managed by an external component.
-	PeeringConditionStatusDisabled PeeringConditionStatusType = "Disabled"
-)
-
-// PeeringType defines the type of peering to be established.
-type PeeringType string
-
-const (
-	// PeeringTypeOutOfBand represents an out-of-band control-plane peering
-	// (i.e., control plane traffic flows outside the network fabric).
-	PeeringTypeOutOfBand PeeringType = "OutOfBand"
-	// PeeringTypeInBand represents an in-band control-plane peering.
-	// (i.e., control plane traffic flows inside the network fabric).
-	PeeringTypeInBand PeeringType = "InBand"
-	// PeeringTypeUnknown represents the empty value for the peering type.
-	PeeringTypeUnknown PeeringType = ""
-)
-
-// PeeringEnabledType indicates the desired state for the peering with this remote cluster.
-type PeeringEnabledType string
-
-const (
-	// PeeringEnabledAuto indicates to use the default settings for the discovery method.
-	// This is useful to track that the user did not set the peering state for that cluster,
-	// if the peering is Auto liqo will use the default for that discovery method:
-	// manual -> No.
-	// incomingPeering -> No.
-	// LAN -> Yes.
-	PeeringEnabledAuto PeeringEnabledType = "Auto"
-	// PeeringEnabledNo indicates to disable the peering with this remote cluster.
-	PeeringEnabledNo PeeringEnabledType = "No"
-	// PeeringEnabledYes indicates to enable the peering with this remote cluster.
-	PeeringEnabledYes PeeringEnabledType = "Yes"
-)
-
 // ForeignClusterSpec defines the desired state of ForeignCluster.
 type ForeignClusterSpec struct {
-	// The type of peering to be established.
-	// +kubebuilder:validation:Enum="OutOfBand";"InBand"
-	// +kubebuilder:default="OutOfBand"
-	// +kubebuilder:validation:Optional
-	PeeringType PeeringType `json:"peeringType,omitempty"`
-
 	// Foreign Cluster Identity.
 	ClusterIdentity ClusterIdentity `json:"clusterIdentity,omitempty"`
-	// Enable the peering process to the remote cluster.
-	// +kubebuilder:validation:Enum="Auto";"No";"Yes"
-	// +kubebuilder:default="Auto"
-	// +kubebuilder:validation:Optional
-	OutgoingPeeringEnabled PeeringEnabledType `json:"outgoingPeeringEnabled"`
-	// Allow the remote cluster to establish a peering with our cluster.
-	// +kubebuilder:validation:Enum="Auto";"No";"Yes"
-	// +kubebuilder:default="Auto"
-	// +kubebuilder:validation:Optional
-	IncomingPeeringEnabled PeeringEnabledType `json:"incomingPeeringEnabled"`
-	// URL where to contact foreign Auth service.
-	// +kubebuilder:validation:Pattern=`https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`
-	ForeignAuthURL string `json:"foreignAuthUrl"`
-	// URL where to contact foreign proxy for the api server. This URL is used when
-	// creating the k8s clients toward the remote cluster.
-	ForeignProxyURL string `json:"foreignProxyUrl,omitempty"`
-	// Indicates if the local cluster has to skip the tls verification over the remote Authentication Service or not.
-	// +kubebuilder:default=true
-	// +kubebuilder:validation:Optional
-	InsecureSkipTLSVerify *bool `json:"insecureSkipTLSVerify"`
-	// If discoveryType is LAN, this indicates the number of seconds after that
-	// this ForeignCluster will be removed if no updates have been received.
-	// +kubebuilder:validation:Minimum=0
-	TTL int `json:"ttl,omitempty"`
 }
 
 // ClusterIdentity contains the information about a remote cluster (ID and Name).
@@ -130,54 +37,124 @@ func (i ClusterIdentity) String() string {
 	return i.ClusterName
 }
 
+// RoleType represents the role of a ForeignCluster.
+type RoleType string
+
+// These are valid roles for a ForeignCluster.
+const (
+	// ConsumerRole represents a cluster that consumes resources from the local cluster.
+	ConsumerRole RoleType = "Consumer"
+	// ProviderRole represents a cluster that provides resources to the local cluster.
+	ProviderRole RoleType = "Provider"
+	// ConsumerAndProviderRole represents a cluster that consumes and provides resources to the local cluster.
+	ConsumerAndProviderRole RoleType = "ConsumerAndProvider"
+	// UnknownRole represents a cluster whose role is unknown.
+	UnknownRole RoleType = "Unknown"
+)
+
 // ForeignClusterStatus defines the observed state of ForeignCluster.
 type ForeignClusterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Role of the ForeignCluster.
+	// +kubebuilder:validation:Enum="Consumer";"Provider";"ConsumerAndProvider";"Unknown"
+	Role RoleType `json:"role"`
 
-	// TenantNamespace names in the peered clusters
-	// +kubebuilder:validation:Optional
-	TenantNamespace TenantNamespaceType `json:"tenantNamespace"`
-
-	// PeeringConditions contains the conditions about the peering related to this
-	// ForeignCluster.
-	PeeringConditions []PeeringCondition `json:"peeringConditions,omitempty"`
+	// Modules contains the configuration of the modules for this foreign cluster.
+	Modules Modules `json:"modules,omitempty"`
 
 	// URL of the forign cluster's API server.
 	// +kubebuilder:validation:Optional
 	APIServerURL string `json:"apiServerUrl,omitempty"`
+
+	// URL where to contact foreign proxy for the api server.
+	// This URL is used when creating the k8s clients toward the remote cluster.
+	// +kubebuilder:validation:Optional
+	ForeignProxyURL string `json:"foreignProxyUrl,omitempty"`
+
+	// TenantNamespace names in the peered clusters
+	// +kubebuilder:validation:Optional
+	TenantNamespace TenantNamespaceType `json:"tenantNamespace"`
 }
 
-// PeeringConditionType represents different conditions that a peering could assume.
-type PeeringConditionType string
+// Modules contains the configuration of the modules for this foreign cluster.
+type Modules struct {
+	Networking     Module `json:"networking"`
+	Authentication Module `json:"authentication"`
+	Offloading     Module `json:"offloading"`
+}
 
-// These are valid conditions of a peering.
+// Module contains the configuration and conditions of a module for a foreign cluster.
+type Module struct {
+	// Enabled indicates if the module is enabled or not.
+	Enabled bool `json:"enabled"`
+	// Conditions contains the status conditions related to the module.
+	Conditions []Condition `json:"conditions,omitempty"`
+}
+
+// ConditionType represents different conditions that a  could assume.
+type ConditionType string
+
+// These are valid type of Conditions.
 const (
-	// OutgoingPeeringCondition informs users about the outgoing peering status.
-	OutgoingPeeringCondition PeeringConditionType = "OutgoingPeering"
-	// IncomingPeeringCondition informs users about the incoming peering status.
-	IncomingPeeringCondition PeeringConditionType = "IncomingPeering"
-	// NetworkStatusCondition informs users about the network status.
-	NetworkStatusCondition PeeringConditionType = "NetworkStatus"
-	// APIServerStatusCondition informs users about the foreign API server status.
-	APIServerStatusCondition PeeringConditionType = "APIServerStatus"
-	// AuthenticationStatusCondition informs users about the Authentication status.
-	AuthenticationStatusCondition PeeringConditionType = "AuthenticationStatus"
-	// ProcessForeignClusterStatusCondition informs users whether the Foreign Cluster is processable.
-	ProcessForeignClusterStatusCondition PeeringConditionType = "ProcessForeignClusterStatus"
+	// NETWORKING
+	// NetworkConnectionStatusCondition shows the network connection status.
+	NetworkConnectionStatusCondition ConditionType = "NetworkConnectionStatus"
+	// NetworkGatewayServerStatusCondition shows the network gateway server status.
+	NetworkGatewayServerStatusCondition ConditionType = "NetworkGatewayServerStatus"
+	// NetworkGatewayClientStatusCondition shows the network gateway client status.
+	NetworkGatewayClientStatusCondition ConditionType = "NetworkGatewayClientStatus"
+
+	// AUTHENTICATION
+	// AuthIdentityControlPlaneStatusCondition shows the status of the ControlPlane Identity.
+	AuthIdentityControlPlaneStatusCondition ConditionType = "AuthIdentityControlPlaneStatus"
+	// AuthTenantStatusCondition shows the status of the Tenant.
+	AuthTenantStatusCondition ConditionType = "AuthTenantStatus"
+
+	// OFFLOADING
+	// OffloadingAPIServerStatusCondition shows the status of the API Server.
+	OffloadingAPIServerStatusCondition ConditionType = "OffloadingAPIServerStatus"
+	// OffloadingVirtualNodeStatusCondition shows the status of a Virtual Node.
+	OffloadingVirtualNodeStatusCondition ConditionType = "OffloadingVirtualNodeStatus"
+	// OffloadingNodeStatusCondition shows the status of a Node.
+	OffloadingNodeStatusCondition ConditionType = "OffloadingNodeStatus"
 )
 
-// PeeringCondition contains details about state of the peering.
-type PeeringCondition struct {
-	// Type of the peering condition.
-	// +kubebuilder:validation:Enum="OutgoingPeering";"IncomingPeering";"NetworkStatus";"APIServerStatus";"AuthenticationStatus";"ProcessForeignClusterStatus"
-	//
-	//nolint:lll // ignore long lines given by Kubebuilder marker annotations
-	Type PeeringConditionType `json:"type"`
+// ConditionStatusType indicates the status of a condition with a remote cluster.
+type ConditionStatusType string
+
+const (
+	// ConditionStatusNone indicates that the condition is not applicable.
+	ConditionStatusNone ConditionStatusType = "None"
+	// ConditionStatusPending indicates that the condition is pending.
+	ConditionStatusPending ConditionStatusType = "Pending"
+	// ConditionStatusEstablished indicates that the condition has been established.
+	ConditionStatusEstablished ConditionStatusType = "Established"
+	// ConditionStatusDisconnecting indicates that the condition is disconnecting.
+	ConditionStatusDisconnecting ConditionStatusType = "Disconnecting"
+	// ConditionStatusDenied indicates that the condition has been denied.
+	ConditionStatusDenied ConditionStatusType = "Denied"
+	// ConditionStatusError indicates that an error has occurred.
+	ConditionStatusError ConditionStatusType = "Error"
+	// ConditionStatusSuccess indicates that the condition is successful.
+	ConditionStatusSuccess ConditionStatusType = "Success"
+	// ConditionStatusExternal indicates that the condition is managed by an external component.
+	ConditionStatusExternal ConditionStatusType = "External"
+	// ConditionStatusReady indicates that the condition is ready.
+	ConditionStatusReady ConditionStatusType = "Ready"
+	// ConditionStatusNotReady indicates that the condition is not ready.
+	ConditionStatusNotReady ConditionStatusType = "NotReady"
+	// ConditionStatusSomeNotReady indicates that not all components of the conditons are ready.
+	ConditionStatusSomeNotReady ConditionStatusType = "SomeNotReady"
+)
+
+// Condition contains details about state of a.
+type Condition struct {
+	// Type of the condition.
+	// +kubebuilder:validation:Enum="NetworkConnectionStatus";"NetworkGatewayServerStatus";"NetworkGatewayClientStatus";"AuthIdentityControlPlaneStatus";"AuthTenantStatus";"OffloadingAPIServerStatus";"OffloadingVirtualNodeStatus";"OffloadingNodeStatus"
+	Type ConditionType `json:"type"`
 	// Status of the condition.
-	// +kubebuilder:validation:Enum="None";"Pending";"Established";"Disconnecting";"Denied";"EmptyDenied";"Error";"Success";"Disabled"
+	// +kubebuilder:validation:Enum="None";"Pending";"Established";"Disconnecting";"Denied";"Error";"Success";"External";"Ready";"NotReady";"SomeNotReady"
 	// +kubebuilder:default="None"
-	Status PeeringConditionStatusType `json:"status"`
+	Status ConditionStatusType `json:"status"`
 	// LastTransitionTime -> timestamp for when the condition last transitioned from one status to another.
 	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
 	// Reason -> Machine-readable, UpperCamelCase text indicating the reason for the condition's last transition.
@@ -200,14 +177,9 @@ type TenantNamespaceType struct {
 // +kubebuilder:subresource:status
 
 // ForeignCluster is the Schema for the foreignclusters API.
-// +kubebuilder:printcolumn:name="Type",type=string,JSONPath=`.spec.peeringType`
+// +kubebuilder:printcolumn:name="Role",type=string,JSONPath=`.status.role`
 // +kubebuilder:printcolumn:name="ClusterID",type=string,priority=1,JSONPath=`.spec.clusterIdentity.clusterID`
 // +kubebuilder:printcolumn:name="ClusterName",type=string,priority=1,JSONPath=`.spec.clusterIdentity.clusterName`
-// +kubebuilder:printcolumn:name="Outgoing peering",type=string,JSONPath=`.status.peeringConditions[?(@.type == 'OutgoingPeering')].status`
-// +kubebuilder:printcolumn:name="Incoming peering",type=string,JSONPath=`.status.peeringConditions[?(@.type == 'IncomingPeering')].status`
-// +kubebuilder:printcolumn:name="Networking",type=string,JSONPath=`.status.peeringConditions[?(@.type == 'NetworkStatus')].status`
-// +kubebuilder:printcolumn:name="API Server",type=string,priority=1,JSONPath=`.status.peeringConditions[?(@.type == 'APIServerStatus')].status`
-// +kubebuilder:printcolumn:name="Authentication",type=string,JSONPath=`.status.peeringConditions[?(@.type == 'AuthenticationStatus')].status`
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
 type ForeignCluster struct {
 	metav1.TypeMeta   `json:",inline"`
