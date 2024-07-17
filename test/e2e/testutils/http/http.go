374f0f67d6f5d261acd7bb7fc9cd70cafb13f38a
Francesco Torta <62566275+fra98@users.noreply.github.com>
2024-07-17 16:23:11 +0200

    Test e2e: added counters on virtualkubet metrics

---
+++ test/e2e/testutils/http/http.go
@@ -0,0 +1,59 @@
+// Copyright 2019-2024 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package http
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+// Client struct to handle http requests.
+type Client struct {
+	Client *http.Client
+}
+
+// NewHTTPClient creates a new HttpClient with a specified timeout.
+func NewHTTPClient(timeout time.Duration) *Client {
+	return &Client{
+		Client: &http.Client{
+			Timeout: timeout,
+		},
+	}
+}
+
+// Curl executes a curl command to the specified target.
+// It returns the response and the body of the response, or an error if the request fails.
+func (hc *Client) Curl(ctx context.Context, url string) (*http.Response, []byte, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", url, http.NoBody)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+
+	resp, err := hc.Client.Do(req)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to execute curl command: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	return resp, body, nil
+}
