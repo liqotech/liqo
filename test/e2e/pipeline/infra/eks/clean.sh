f6ad3c8c84c0d9cfc104ccf7ee494105e1c3c0a5
Alessandro Olivero <aleoli96@gmail.com>
2024-07-23 12:25:06 +0200

    e2e plugin for eks

---
+++ test/e2e/pipeline/infra/eks/clean.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This scripts expects the following variables to be set:
+# CLUSTER_NUMBER        -> the number of liqo clusters
+# K8S_VERSION           -> the Kubernetes version
+# CNI                   -> the CNI plugin used
+# TMPDIR                -> the directory where the test-related files are stored
+# BINDIR                -> the directory where the test-related binaries are stored
+# TEMPLATE_DIR          -> the directory where to read the cluster templates
+# NAMESPACE             -> the namespace where liqo is running
+# KUBECONFIGDIR         -> the directory where the kubeconfigs are stored
+# LIQO_VERSION          -> the liqo version to test
+# INFRA                 -> the Kubernetes provider for the infrastructure
+# LIQOCTL               -> the path where liqoctl is stored
+# KUBECTL               -> the path where kubectl is stored
+# EKSCTL                -> the path where eksctl is stored
+# AWS_CLI               -> the path where aws-cli is stored
+# POD_CIDR_OVERLAPPING  -> the pod CIDR of the clusters is overlapping
+# CLUSTER_TEMPLATE_FILE -> the file where the cluster template is stored
+
+set -e           # Fail in case of error
+set -o nounset   # Fail if undefined variables are used
+set -o pipefail  # Fail if one of the piped commands fails
+
+error() {
+   local sourcefile=$1
+   local lineno=$2
+   echo "An error occurred at $sourcefile:$lineno."
+}
+trap 'error "${BASH_SOURCE}" "${LINENO}"' ERR
+
+CLUSTER_NAME=cluster
+RUNNER_NAME=${RUNNER_NAME:-"test"}
+CLUSTER_NAME="${RUNNER_NAME}-${CLUSTER_NAME}"
+
+PIDS=()
+
+# Cleaning all remaining clusters
+for i in $(seq 1 "${CLUSTER_NUMBER}")
+do
+    # if the cluster exists, delete it
+    if "${EKSCTL}" get cluster --name "${CLUSTER_NAME}${i}" --region "eu-central-1" &> /dev/null; then
+        echo "Deleting cluster ${CLUSTER_NAME}${i}"
+    else
+        echo "Cluster ${CLUSTER_NAME}${i} does not exist"
+        continue
+    fi
+    "${EKSCTL}" delete cluster --name "${CLUSTER_NAME}${i}" --region "eu-central-1" --wait --force &
+    PIDS+=($!)
+done
+
+for PID in "${PIDS[@]}"; do
+    wait "${PID}"
+done
