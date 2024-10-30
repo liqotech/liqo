59134f9fd33120ae975b572d7e3910229560d69c
Francesco Torta <62566275+fra98@users.noreply.github.com>
2024-10-30 15:59:50 +0100

    feat: ipam sync routine

--- pkg/ipam/initialize.go
+++ pkg/ipam/initialize.go
@@ -18,18 +18,10 @@ import (
 	"context"
 
 	klog "k8s.io/klog/v2"
-
-	ipamv1alpha1 "github.com/liqotech/liqo/apis/ipam/v1alpha1"
-	"github.com/liqotech/liqo/pkg/consts"
 )
 
 // +kubebuilder:rbac:groups=ipam.liqo.io,resources=ips,verbs=get;list;watch
 // +kubebuilder:rbac:groups=ipam.liqo.io,resources=networks,verbs=get;list;watch
-
-type ipCidr struct {
-	ip   string
-	cidr string
-}
 
 func (lipam *LiqoIPAM) initialize(ctx context.Context) error {
 	if err := lipam.initializeNetworks(ctx); err != nil {
@@ -45,15 +37,16 @@ func (lipam *LiqoIPAM) initialize(ctx context.Context) error {
 }
 
 func (lipam *LiqoIPAM) initializeNetworks(ctx context.Context) error {
-	// Initialize the networks.
-	nets, err := lipam.getReservedNetworks(ctx)
+	// List all networks present in the cluster.
+	nets, err := listNetworksOnCluster(ctx, lipam.Client)
 	if err != nil {
 		return err
 	}
 
+	// Initialize the networks.
 	for _, net := range nets {
 		if err := lipam.reserveNetwork(net); err != nil {
-			klog.Errorf("Failed to reserve network %s: %v", net, err)
+			klog.Errorf("Failed to reserve network %q: %v", net, err)
 			return err
 		}
 	}
@@ -62,86 +55,19 @@ func (lipam *LiqoIPAM) initializeNetworks(ctx context.Context) error {
 }
 
 func (lipam *LiqoIPAM) initializeIPs(ctx context.Context) error {
-	// Initialize the IPs.
-	ips, err := lipam.getReservedIPs(ctx)
+	// List all IPs present in the cluster.
+	ips, err := listIPsOnCluster(ctx, lipam.Client)
 	if err != nil {
 		return err
 	}
 
+	// Initialize the IPs.
 	for _, ip := range ips {
-		if err := lipam.reserveIP(ip.ip, ip.cidr); err != nil {
-			klog.Errorf("Failed to reserve IP %s in network %s: %v", ip.ip, ip.cidr, err)
+		if err := lipam.reserveIP(ip); err != nil {
+			klog.Errorf("Failed to reserve IP %q (network %q): %v", ip.ip, ip.cidr, err)
 			return err
 		}
 	}
 
-	return nil
-}
-
-func (lipam *LiqoIPAM) getReservedNetworks(ctx context.Context) ([]string, error) {
-	var nets []string
-	var networks ipamv1alpha1.NetworkList
-	if err := lipam.Options.Client.List(ctx, &networks); err != nil {
-		return nil, err
-	}
-
-	for i := range networks.Items {
-		net := &networks.Items[i]
-
-		var cidr string
-		switch {
-		case net.Labels != nil && net.Labels[consts.NetworkNotRemappedLabelKey] == consts.NetworkNotRemappedLabelValue:
-			cidr = net.Spec.CIDR.String()
-		default:
-			cidr = net.Status.CIDR.String()
-		}
-		if cidr == "" {
-			klog.Warningf("Network %s has no CIDR", net.Name)
-			continue
-		}
-
-		nets = append(nets, cidr)
-	}
-
-	return nets, nil
-}
-
-func (lipam *LiqoIPAM) getReservedIPs(ctx context.Context) ([]ipCidr, error) {
-	var ips []ipCidr
-	var ipList ipamv1alpha1.IPList
-	if err := lipam.Options.Client.List(ctx, &ipList); err != nil {
-		return nil, err
-	}
-
-	for i := range ipList.Items {
-		ip := &ipList.Items[i]
-
-		address := ip.Status.IP.String()
-		if address == "" {
-			klog.Warningf("IP %s has no address", ip.Name)
-			continue
-		}
-
-		cidr := ip.Status.CIDR.String()
-		if cidr == "" {
-			klog.Warningf("IP %s has no CIDR", ip.Name)
-			continue
-		}
-
-		ips = append(ips, ipCidr{ip: address, cidr: cidr})
-	}
-
-	return ips, nil
-}
-
-func (lipam *LiqoIPAM) reserveNetwork(cidr string) error {
-	// TODO: Reserve the network.
-	klog.Infof("Reserved network %s", cidr)
-	return nil
-}
-
-func (lipam *LiqoIPAM) reserveIP(ip, cidr string) error {
-	// TODO: Reserve the IP.
-	klog.Infof("Reserved IP %s in network %s", ip, cidr)
 	return nil
 }
---
+++ pkg/ipam/networks.go
@@ -0,0 +1,86 @@
+// Copyright 2019-2024 The Liqo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ipam
+
+import (
+	"context"
+	"time"
+
+	klog "k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	ipamv1alpha1 "github.com/liqotech/liqo/apis/ipam/v1alpha1"
+	"github.com/liqotech/liqo/pkg/consts"
+)
+
+type networkInfo struct {
+	cidr              string
+	creationTimestamp time.Time
+}
+
+// reserveNetwork reserves a network, saving it in the cache.
+func (lipam *LiqoIPAM) reserveNetwork(cidr string) error {
+	lipam.mutex.Lock()
+	defer lipam.mutex.Unlock()
+
+	nwI := networkInfo{
+		cidr:              cidr,
+		creationTimestamp: time.Now(),
+	}
+	if lipam.cacheNetworks == nil {
+		lipam.cacheNetworks = make(map[string]networkInfo)
+	}
+	lipam.cacheNetworks[cidr] = nwI
+
+	klog.Infof("Reserved network %q", cidr)
+	return nil
+}
+
+// freeNetwork frees a network, removing it from the cache.
+func (lipam *LiqoIPAM) freeNetwork(cidr string) {
+	lipam.mutex.Lock()
+	defer lipam.mutex.Unlock()
+
+	delete(lipam.cacheNetworks, cidr)
+	klog.Infof("Freed network %q", cidr)
+}
+
+func listNetworksOnCluster(ctx context.Context, cl client.Client) ([]string, error) {
+	var nets []string
+	var networks ipamv1alpha1.NetworkList
+	if err := cl.List(ctx, &networks); err != nil {
+		return nil, err
+	}
+
+	for i := range networks.Items {
+		net := &networks.Items[i]
+
+		var cidr string
+		switch {
+		case net.Labels != nil && net.Labels[consts.NetworkNotRemappedLabelKey] == consts.NetworkNotRemappedLabelValue:
+			cidr = net.Spec.CIDR.String()
+		default:
+			cidr = net.Status.CIDR.String()
+		}
+		if cidr == "" {
+			klog.Warningf("Network %q has no CIDR", net.Name)
+			continue
+		}
+
+		nets = append(nets, cidr)
+	}
+
+	return nets, nil
+}
