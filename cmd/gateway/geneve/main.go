3ed7893cb74257a9cfa1bedca917319d6cd4c75e
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2025-01-09 15:34:58 +0100

    feat: fabric and gateway health probe

--- cmd/gateway/geneve/main.go
+++ cmd/gateway/geneve/main.go
@@ -25,6 +25,7 @@ import (
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
@@ -93,6 +94,14 @@ func run(cmd *cobra.Command, _ []string) error {
 	})
 	if err != nil {
 		return fmt.Errorf("unable to create manager: %w", err)
+	}
+
+	// Register the healthiness probes.
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		return fmt.Errorf("unable to set up healthz probe: %w", err)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		return fmt.Errorf("unable to set up readyz probe: %w", err)
 	}
 
 	inr, err := geneve.NewInternalNodeReconciler(
--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -29,6 +29,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
@@ -158,6 +159,14 @@ func run(cmd *cobra.Command, _ []string) error {
 	})
 	if err != nil {
 		return fmt.Errorf("unable to create manager: %w", err)
+	}
+
+	// Register the healthiness probes.
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		return fmt.Errorf("unable to set up healthz probe: %w", err)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		return fmt.Errorf("unable to set up readyz probe: %w", err)
 	}
 
 	if connoptions.EnableConnectionController {
