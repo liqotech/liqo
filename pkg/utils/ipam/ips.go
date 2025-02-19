4c1d17e1adb615f2d2fc91d690857bdca0e5e421
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2025-02-19 10:26:01 +0100

    chore: bump gci from 0.13.4 to 0.13.5

--- pkg/utils/ipam/ips.go
+++ pkg/utils/ipam/ips.go
@@ -17,10 +17,9 @@ package ipam
 import (
 	"encoding/binary"
 	"fmt"
+	"iter"
 	"net"
 	"net/netip"
-
-	"iter"
 
 	ipamv1alpha1 "github.com/liqotech/liqo/apis/ipam/v1alpha1"
 	networkingv1beta1 "github.com/liqotech/liqo/apis/networking/v1beta1"
