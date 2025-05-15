bceba7524ed6ebae0237f7156c3c90d05b7b630c
Claudio Lorina <claudio.lorina@gmail.com>
2025-05-15 15:23:24 +0200

    feat: enable templating in global labels and annotations

--- pkg/utils/resource/wrappers.go
+++ pkg/utils/resource/wrappers.go
@@ -16,10 +16,16 @@ package resource
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
+
+var knownVariables = []string{"namespace", "name", "kind", "group"}
 
 // CreateOrUpdate is a wrapper around controllerutil.CreateOrUpdate that automatically adds global labels and annotations.
 // It takes the same parameters as controllerutil.CreateOrUpdate plus an optional list of additional labels to merge.
@@ -32,13 +38,19 @@ func CreateOrUpdate(ctx context.Context, c client.Client, obj client.Object,
 			return err
 		}
 
+		// Build the variables map from the object to replace variables in global labels and annotations.
+		variablesMap, err := buildVariablesMap(obj, c.Scheme())
+		if err != nil {
+			return fmt.Errorf("unable replace variable in global labels and annotations: %w", err)
+		}
+
 		// Then add global labels and any additional labels
 		if obj.GetLabels() == nil {
 			obj.SetLabels(make(map[string]string))
 		}
 		labels := obj.GetLabels()
 		for k, v := range GetGlobalLabels() {
-			labels[k] = v
+			labels[k] = replaceVariables(v, variablesMap)
 		}
 		obj.SetLabels(labels)
 
@@ -48,7 +60,7 @@ func CreateOrUpdate(ctx context.Context, c client.Client, obj client.Object,
 		}
 		annotations := obj.GetAnnotations()
 		for k, v := range GetGlobalAnnotations() {
-			annotations[k] = v
+			annotations[k] = replaceVariables(v, variablesMap)
 		}
 		obj.SetAnnotations(annotations)
 
@@ -56,4 +68,43 @@ func CreateOrUpdate(ctx context.Context, c client.Client, obj client.Object,
 	}
 
 	return controllerutil.CreateOrUpdate(ctx, c, obj, wrappedMutateFn)
+}
+
+func getVar(variable string, obj client.Object, kind schema.GroupVersionKind) string {
+	switch variable {
+	case "namespace":
+		return obj.GetNamespace()
+	case "name":
+		return obj.GetName()
+	case "kind":
+		return kind.Kind
+	case "group":
+		return kind.Group
+	}
+
+	return ""
+}
+
+func buildVariablesMap(obj client.Object, scheme *runtime.Scheme) (map[string]string, error) {
+	variables := make(map[string]string)
+	groupKind, _, err := scheme.ObjectKinds(obj)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve object kind: %w", err)
+	}
+
+	for _, variable := range knownVariables {
+		variables[variable] = getVar(variable, obj, groupKind[0])
+	}
+	return variables, nil
+}
+
+// replaceVariables replace the variables in the given string with the corresponding values in the object.
+func replaceVariables(s string, variablesMap map[string]string) string {
+	// Replace the variables in the string with the corresponding values in the object
+	for varName, varValue := range variablesMap {
+		s = strings.ReplaceAll(s, "${"+varName+"}", varValue)
+	}
+
+	return s
 }
