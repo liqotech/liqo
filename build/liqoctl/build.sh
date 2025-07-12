d049864c4f00b639a63793ea0584edc3db9f3e02
Francesco Cheinasso <33266330+cheina97@users.noreply.github.com>
2025-07-12 14:49:23 +0000

    ci: new build pipeline

--- build/liqoctl/build.sh
+++ build/liqoctl/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e           # Fail in case of error
-set -o nounset   # Fail if undefined variables are used
-set -o pipefail  # Fail if one of the piped commands fails
+set -e          # Fail in case of error
+set -o nounset  # Fail if undefined variables are used
+set -o pipefail # Fail if one of the piped commands fails
 
 error() {
    local sourcefile=$1
@@ -11,18 +11,19 @@ error() {
 }
 trap 'error "${BASH_SOURCE}" "${LINENO}"' ERR
 
-GO_VERSION="1.24"
-
 CGO_ENABLED="${CGO_ENABLED:-0}"
 GOOS="${GOOS:-linux}"
 GOARCH="${GOARCH:-amd64}"
 LIQOCTLVERSION="${LIQOCTLVERSION:-dev}"
 
-docker run -v "$PWD:/liqo" -w /liqo -e="CGO_ENABLED=${CGO_ENABLED}" \
-   -e "GOOS=${GOOS}" -e "GOARCH=${GOARCH}" \
-   -e "LIQOCTLVERSION=${LIQOCTLVERSION}" \
-   --rm "golang:${GO_VERSION}" \
-   sh -c "go mod tidy && go build -o \"./liqoctl-${GOOS}-${GOARCH}\" \
-   -ldflags=\"-s -w -X 'github.com/liqotech/liqo/pkg/liqoctl/version.LiqoctlVersion=${LIQOCTLVERSION}'\" \
+echo "Downloading Go modules..."
+start_time=$(date +%s)
+go mod download
+end_time=$(date +%s)
+elapsed=$((end_time - start_time))
+echo "Go modules downloaded in ${elapsed} seconds."
+
+CGO_ENABLED=0 go build -o "./liqoctl-${GOOS}-${GOARCH}" \
+   -ldflags="-s -w -X 'github.com/liqotech/liqo/pkg/liqoctl/version.LiqoctlVersion=${LIQOCTLVERSION}'" \
    -buildvcs=false \
-   ./cmd/liqoctl"
+   ./cmd/liqoctl
